70b6548d25b38c78a108a2a2d951d13d8bd4c486
Jérôme GIACOMINI <archeone@gmail.com>
2018-10-06 09:51:39 +0200

    Fix a bug when user back without taking a picture

--- UI/AndroidCustomCamera/AndroidCustomCamera/AndroidCustomCamera.Shared/MainPage.xaml.cs
+++ UI/AndroidCustomCamera/AndroidCustomCamera/AndroidCustomCamera.Shared/MainPage.xaml.cs
@@ -1,32 +1,16 @@
-﻿using Android.App;
-using Android.Content;
+﻿using Android.Content;
 using Android.Graphics;
-using Android.OS;
 using Android.Provider;
-using Android.Runtime;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Uno.UI;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace AndroidCustomCamera
 {
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
-	public sealed partial class MainPage : Page
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
 	{
 		public MainPage()
 		{
@@ -37,9 +21,17 @@ namespace AndroidCustomCamera
 
 		private void Current_ActivityResult(object sender, BaseActivity.ActivityResultEventArgs e)
 		{
-			var bitmap = (Bitmap)e.Data.Extras.Get("data");
 
-			image.Source = bitmap;
+            if (e.Data != null)
+            {
+                var bitmap = (Bitmap)e.Data.Extras.Get("data");
+
+                image.Source = bitmap;
+            }
+            else
+            {
+                image.Source = null;
+            }
 		}
 
 		public void button_Click(object sender, RoutedEventArgs e)
