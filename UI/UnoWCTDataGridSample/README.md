a208681c5c8acc10632daafa98805741ada3e882
David Oliver <david.oliver@nventive.com>
2020-04-22 13:18:13 -0400

    Add WCT DataGrid sample
    Extracts sample from WCT source samples app

---
+++ UI/UnoWCTDataGridSample/README.md
@@ -0,0 +1,5 @@
+# Windows Community Toolkit DataGrid Sample
+
+This is a simple standalone app demonstrating the [Windows Community Toolkit DataGrid](https://docs.microsoft.com/en-us/windows/communitytoolkit/controls/datagrid) integrated into an [Uno Platform](https://platform.uno/) app.
+
+The `DataGrid` XAML code and model data is taken from the [WCT Samples App](https://github.com/unoplatform/Uno.WindowsCommunityToolkit/blob/uno/Microsoft.Toolkit.Uwp.SampleApp/SamplePages/DataGrid/DataGridCode.bind). 
---
+++ UI/UnoWCTDataGridSample/WCTTestbed2/WCTTestbed2.Shared/StreamHelperEx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCTTestbed2
+{
+	public static class StreamHelperEx
+	{
+		/// <summary>
+		/// Return a stream to a specified file from the installation folder.
+		/// </summary>
+		/// <param name="assemblyType">The owner type for the embedded file</param>
+		/// <param name="fileName">Relative name of the file to open. Can contains subfolders.</param>
+		/// <returns>File stream</returns>
+		public static async Task<Stream> GetEmbeddedFileStreamAsync(Type assemblyType, string fileName)
+		{
+			await Task.Yield();
+
+			var manifestName = assemblyType.GetTypeInfo().Assembly
+				.GetManifestResourceNames()
+				.FirstOrDefault(n => n.EndsWith(fileName.Replace(" ", "_"), StringComparison.OrdinalIgnoreCase));
+
+			if (manifestName == null)
+			{
+				throw new InvalidOperationException($"Failed to find resource [{fileName}]");
+			}
+
+			return assemblyType.GetTypeInfo().Assembly.GetManifestResourceStream(manifestName);
+		}
+	}
+}
