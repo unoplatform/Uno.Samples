01aa696dac124de377ca2f42981be5ea29de0ef9
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2020-07-16 12:00:47 -0400

    fix(android): Use newer NativeApplication ctor

--- UI/ControlLibrary/Apps/App.ControlLibrary.Droid/Main.cs
+++ UI/ControlLibrary/Apps/App.ControlLibrary.Droid/Main.cs
@@ -21,7 +21,7 @@ namespace Uno.Sample.ControlLibrary.Droid
 	public class Application : Windows.UI.Xaml.NativeApplication
 	{
 		public Application(IntPtr javaReference, JniHandleOwnership transfer)
-			: base(new App(), javaReference, transfer)
+			: base(() => new App(), javaReference, transfer)
 		{
 		}
 	}
