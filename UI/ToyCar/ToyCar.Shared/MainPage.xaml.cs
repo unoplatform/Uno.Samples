ff67a8d34238ffca5acf774f020e3c0bc71e76ca
Agnès Zitte <agnes.zitte@nventive.com>
2020-08-12 05:53:04 -0400

    feat: Update ToyCar sample with more content

--- UI/ToyCar/ToyCar.Shared/MainPage.xaml.cs
+++ UI/ToyCar/ToyCar.Shared/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 namespace ToyCar
@@ -19,6 +20,7 @@ namespace ToyCar
 	{
 		private TranslateTransform dragUpperTranslation;
 		private TranslateTransform dragLowerTranslation;
+		private bool isAnimationStarted = false;
 
 		public MainPage()
 		{
@@ -34,19 +36,36 @@ namespace ToyCar
 			dragUpperTranslation = new TranslateTransform();
 			dragLowerTranslation = new TranslateTransform();
 
-			// Apply the translation to the Rectangles.
+			// Apply the translations to the elements.
 			TouchRectangle.RenderTransform = this.dragUpperTranslation;
 			DragLeftCar.RenderTransform = this.dragLowerTranslation;
 			DragRightCar.RenderTransform = this.dragLowerTranslation;
+			FeaturesPanel.RenderTransform = this.dragLowerTranslation;
 
 			Unloaded += OnUnloaded;
 		}
 
 		private void MainRoot_SizeChanged(object sender, SizeChangedEventArgs e)
 		{
-			DragRightCar.Margin = new Thickness(-(MainRoot.ActualWidth / 2), 0, 0, 0);
+			// Making sure we are resetting the X axis and the Y axis if the MainRoot is resized (Can be reviewed in the long term).
 			dragUpperTranslation.X = 0;
 			dragLowerTranslation.X = 0;
+			dragLowerTranslation.Y = dragUpperTranslation.Y;
+
+			// Making sure the car animation is stopped.
+			CarStoryboard.Stop();
+
+			// Making sure the scene animation is paused if it was already started.
+			if (isAnimationStarted)
+			{
+				SceneStoryboard.Pause();
+			}
+
+			// Ajusting the margins on the right side so it will be at the same level as the margins on the left side.
+			DragLeftCar.Margin = new Thickness(MainRoot.ActualWidth * 0.1, 0, 0, 0);
+			TouchRectangle.Margin = new Thickness(MainRoot.ActualWidth * 0.1, 0, 0, 0);
+			DragRightCar.Margin = new Thickness(-((MainRoot.ActualWidth / 2) - (MainRoot.ActualWidth * 0.1)), 0, 0, 0);
+			FeaturesPanel.Margin = new Thickness(-((MainRoot.ActualWidth / 2) - (MainRoot.ActualWidth * 0.1)) - 180, 0, 0, 0);
 		}
 
 		private void OnUnloaded(object sender, RoutedEventArgs e)
@@ -69,19 +88,52 @@ namespace ToyCar
 				// Move the upper touch rectangle along the x-axis.
 				dragUpperTranslation.X += e.Delta.Translation.X;
 
-				if (dragUpperTranslation.X > 0 && dragUpperTranslation.X < (MainRoot.ActualWidth * 0.7))
+				if (dragUpperTranslation.X >= 0 && dragUpperTranslation.X < (MainRoot.ActualWidth * 0.65))
 				{
 					//Making sure the SketchCar is not bouncing and rotating wheels under this threshold.
 					CarStoryboard.Stop();
-					// Move the lower cars along the x-axis.
+
+					// Making sure the scene animation is paused at the same time if it was already started.
+					if (isAnimationStarted)
+					{
+						SceneStoryboard.Pause();
+					}
+
+					// Move the lower cars along the x-axis and keeping the same upper y-axis.
 					dragLowerTranslation.X = dragUpperTranslation.X;
+					dragLowerTranslation.Y = dragUpperTranslation.Y;
+				}
+				else if (dragUpperTranslation.X >= (MainRoot.ActualWidth * 0.65))
+				{
+					// Keep the upper rectangle at the same position as the lower rectangle.
+					dragUpperTranslation.X = dragLowerTranslation.X;
+					dragUpperTranslation.Y = dragLowerTranslation.Y;
+
+					//Starting the SketchCar bouncing and rotating wheels animations above the threshold.
+					CarStoryboard.Begin();
+
+					// Making sure the scene animation begin for the first time or resume if paused.
+					if (isAnimationStarted)
+					{
+						SceneStoryboard.Resume();
+					}
+					else 
+					{ 
+						SceneStoryboard.Begin();
+						isAnimationStarted = true;
+					}
 				}
 				else
 				{
 					// Keep the upper rectangle at the same position as the lower rectangle.
 					dragUpperTranslation.X = dragLowerTranslation.X;
-					//Starting the SketchCar bouncing and rotating wheels animations. 
-					CarStoryboard.Begin();
+					dragUpperTranslation.Y = dragLowerTranslation.Y;
+
+					// Making sure the scene animation is paused at the same time if it was already started.
+					if (isAnimationStarted)
+					{
+						SceneStoryboard.Pause();
+					}
 				}
 			}
 		}
