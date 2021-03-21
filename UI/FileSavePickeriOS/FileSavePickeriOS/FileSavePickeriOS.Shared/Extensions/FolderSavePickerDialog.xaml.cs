17108081074fbd691afbe127a79e4fd7ffb9796d
Martin Zikmund <martin@zikmund.dev>
2021-03-21 19:29:07 +0100

    Folder-based save picker for iOS

---
+++ UI/FileSavePickeriOS/FileSavePickeriOS/FileSavePickeriOS.Shared/Extensions/FolderSavePickerDialog.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace FileSavePickeriOS.Extensions
+{
+    public sealed partial class FolderSavePickerDialog : ContentDialog
+    {
+        private const string SaveResource = "FolderSavePickerSave";
+        private const string CancelResource = "FolderSavePickerCancel";
+        private const string TitleResource = "FolderSavePickerTitle";
+        private const string FileNameResource = "FolderSavePickerFileName";
+        private const string PickFolderResource = "FolderSavePickerPickFolder";
+        private const string BrowseResource = "FolderSavePickerBrowse";
+        private const string FileTypeResource = "FolderSavePickerFileType";
+
+        private static Lazy<ResourceLoader> _resourceLoader = new Lazy<ResourceLoader>(() => ResourceLoader.GetForViewIndependentUse());
+        private StorageFolder _pickedFolder;
+        private string[] _existingFileNames = Array.Empty<string>();
+
+        public FolderSavePickerDialog()
+        {
+            this.InitializeComponent();
+            Title = _resourceLoader.Value.GetString(TitleResource);
+            DefaultButton = ContentDialogButton.Primary;
+            PrimaryButtonText = _resourceLoader.Value.GetString(SaveResource);
+            SecondaryButtonText = _resourceLoader.Value.GetString(CancelResource);
+            FileNameSuggestBox.Header = _resourceLoader.Value.GetString(FileNameResource);
+            PickButton.Content = _resourceLoader.Value.GetString(BrowseResource);
+            PickFolderLabel.Text = _resourceLoader.Value.GetString(PickFolderResource);
+            FileTypeComboBox.Header = _resourceLoader.Value.GetString(FileTypeResource);
+
+            Loaded += FolderSavePickerDialog_Loaded;
+        }
+
+        private void FolderSavePickerDialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdatePickedFolderDisplay();
+        }
+
+        public string PickedFileName { get; private set; }
+
+        public StorageFolder PickedFolder
+        {
+            get => _pickedFolder;
+            private set
+            {
+                _pickedFolder = value;
+                UpdatePickedFolderDisplay();
+            }
+        }
+
+        public string SuggestedFileName
+        {
+            get => FileNameSuggestBox.Text;
+            set => FileNameSuggestBox.Text = value;
+        }
+
+        public void SetFileTypeChoices(IDictionary<string, IList<string>> fileTypeChoices)
+        {
+            var items = new List<FileTypeChoiceListItem>();
+            foreach (var choice in fileTypeChoices)
+            {
+                var extensions = string.Join(",", choice.Value.Select(extension => $"*{extension}"));
+                var label = $"{choice.Key} ({extensions})";
+                items.Add(new FileTypeChoiceListItem(label, choice.Value));
+            }
+            FileTypeComboBox.ItemsSource = items;
+            FileTypeComboBox.SelectedItem = items.FirstOrDefault();
+        }
+
+        private void SaveButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            if (PickedFolder == null)
+            {
+                return;
+            }
+
+            IList<string> allowedExtensions = Array.Empty<string>();
+            if (FileTypeComboBox.SelectedItem is FileTypeChoiceListItem selectedFileTypeChoice)
+            {
+                allowedExtensions = selectedFileTypeChoice.Extensions;
+            }
+
+            bool needsExtension = false;
+            var fileName = FileNameSuggestBox.Text;
+            if (Path.HasExtension(fileName))
+            {
+                var extension = Path.GetExtension(fileName);
+                if (!allowedExtensions.Any(e => e.EndsWith(extension, StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    needsExtension = true;
+                }
+            }
+            else
+            {
+                needsExtension = true;
+            }
+
+            if (needsExtension)
+            {
+                // Entered extension is not allowed, append from the list
+                var firstExtension = allowedExtensions.FirstOrDefault();
+                if (firstExtension != null)
+                {
+                    if (fileName.EndsWith('.'))
+                    {
+                        fileName = fileName.Substring(0, fileName.Length - 1);
+                    }
+
+                    fileName += firstExtension;
+                }
+            }
+
+            PickedFileName = fileName;
+        }
+
+        private void CancelButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            PickedFileName = null;
+        }
+
+        private async void PickButtonClick(object sender, RoutedEventArgs e)
+        {
+            var folderPicker = new FolderPicker();
+            folderPicker.FileTypeFilter.Add("*");
+            var pickedFolder = await folderPicker.PickSingleFolderAsync();
+            if (pickedFolder != null)
+            {
+                var filesInFolder = await pickedFolder.GetFilesAsync();
+                _existingFileNames = filesInFolder.Select(f => f.Name).ToArray();
+            }
+            PickedFolder = pickedFolder;
+        }
+
+        private void FileNameSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            var currentFileTypeChoice = (FileTypeChoiceListItem)FileTypeComboBox.SelectedItem;
+            if (args.Reason != AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                return;
+            }
+
+            var currentText = sender.Text;
+            var matchingFileNames = _existingFileNames
+                .Where(n => n != null && n.Contains(currentText, StringComparison.CurrentCultureIgnoreCase) &&
+                    (!Path.HasExtension(n) ||
+                     currentFileTypeChoice.Extensions.Contains(Path.GetExtension(n))))
+                .Take(5)
+                .ToArray();
+
+            sender.ItemsSource = matchingFileNames;
+        }
+
+        private void UpdatePickedFolderDisplay()
+        {
+            IsPrimaryButtonEnabled = _pickedFolder != null;
+            PickFolderLabel.Visibility = _pickedFolder != null ? Visibility.Collapsed : Visibility.Visible;
+            PickedFolderNameLabel.Text = _pickedFolder?.DisplayName ?? string.Empty;
+        }
+    }
+
+    internal class FileTypeChoiceListItem
+    {
+        public FileTypeChoiceListItem(string label, IList<string> extensions)
+        {
+            Label = label;
+            Extensions = extensions;
+        }
+
+        public string Label { get; }
+
+        public IList<string> Extensions { get; }
+    }
+}
---
+++ UI/FileSavePickeriOS/FileSavePickeriOS/FileSavePickeriOS.Shared/Extensions/FolderSavePickerExtension.cs
@@ -0,0 +1,131 @@
+﻿#if __IOS__
+
+using FileSavePickeriOS.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Uno.Extensions.Storage.Pickers;
+using Windows.ApplicationModel.Resources;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml.Controls;
+
+namespace Custom.Pickers
+{
+    public class FolderSavePickerExtension : IFileSavePickerExtension
+    {
+        private const string FolderExistsTitleResource = "FolderSavePickerFolderExistsTitle";
+        private const string FileExistsTitleResource = "FolderSavePickerFileExistsTitle";
+        private const string FolderExistsTextResource = "FolderSavePickerFolderExistsText";
+        private const string FileExistsTextResource = "FolderSavePickerFileExistsText";
+        private const string CreateFailedTitleResource = "FolderSavePickerCreateFailedTitle";
+        private const string CreateFailedTextResource = "FolderSavePickerCreateFailedText";
+
+        private const string YesResource = "FolderSavePickerYes";
+        private const string NoResource = "FolderSavePickerNo";
+        private const string OkResource = "FolderSavePickerOk";
+
+        private readonly FileSavePicker _fileSavePicker;
+        private readonly ResourceLoader _resourceLoader = ResourceLoader.GetForViewIndependentUse();
+
+        public FolderSavePickerExtension(object owner)
+        {
+            if (!(owner is FileSavePicker picker))
+            {
+                throw new ArgumentException($"Owner of {nameof(FolderSavePickerExtension)} must be a {nameof(FileSavePicker)}");
+            }
+
+            _fileSavePicker = picker;
+        }
+
+        public async Task<StorageFile> PickSaveFileAsync(CancellationToken token)
+        {
+            var dialog = new FolderSavePickerDialog();
+
+            if (!string.IsNullOrEmpty(_fileSavePicker.CommitButtonText))
+            {
+                dialog.PrimaryButtonText = _fileSavePicker.CommitButtonText;
+            }
+
+            if (!string.IsNullOrEmpty(_fileSavePicker.SuggestedFileName))
+            {
+                dialog.SuggestedFileName = _fileSavePicker.SuggestedFileName;
+            }
+
+            dialog.SetFileTypeChoices(_fileSavePicker.FileTypeChoices);           
+
+            do
+            {
+                await dialog.ShowAsync();
+
+                var pickConfirmed = true;
+
+                var fileName = dialog.PickedFileName;
+                var pickedFolder = dialog.PickedFolder;
+                if (fileName != null)
+                {
+                    var item = await dialog.PickedFolder.TryGetItemAsync(fileName);
+                    if (item != null)
+                    {
+                        var itemExistsDialog = new ContentDialog();
+                        if (item.IsOfType(StorageItemTypes.Folder))
+                        {
+                            itemExistsDialog.Title = _resourceLoader.GetString(FolderExistsTitleResource);
+                            itemExistsDialog.Content = string.Format(_resourceLoader.GetString(FolderExistsTextResource), fileName, pickedFolder.DisplayName);
+                            itemExistsDialog.DefaultButton = ContentDialogButton.Primary;
+                            itemExistsDialog.PrimaryButtonText = _resourceLoader.GetString(OkResource);
+                            itemExistsDialog.DefaultButton = ContentDialogButton.Primary;
+                            await itemExistsDialog.ShowAsync();
+                            pickConfirmed = false;
+                        }
+                        else
+                        {
+                            itemExistsDialog.Title = _resourceLoader.GetString(FileExistsTitleResource);
+                            itemExistsDialog.Content = string.Format(_resourceLoader.GetString(FileExistsTextResource), fileName, pickedFolder.DisplayName);
+                            itemExistsDialog.DefaultButton = ContentDialogButton.Primary;
+                            itemExistsDialog.PrimaryButtonText = _resourceLoader.GetString(YesResource);
+                            itemExistsDialog.SecondaryButtonText = _resourceLoader.GetString(NoResource);
+                            itemExistsDialog.DefaultButton = ContentDialogButton.Secondary;
+                            var result = await itemExistsDialog.ShowAsync();
+                            if (result != ContentDialogResult.Primary)
+                            {
+                                pickConfirmed = false;
+                            }
+                        }
+                    }
+                }
+
+                if (pickConfirmed)
+                {
+                    if (dialog.PickedFileName == null)
+                    {
+                        return null;
+                    }
+
+                    try
+                    {
+                        return await dialog.PickedFolder.CreateFileAsync(dialog.PickedFileName, CreationCollisionOption.ReplaceExisting);
+                    }
+                    catch (Exception)
+                    {
+                        var saveFailedDialog = new ContentDialog
+                        {
+                            PrimaryButtonText = _resourceLoader.GetString(OkResource),
+                            DefaultButton = ContentDialogButton.Primary,
+                            Title = _resourceLoader.GetString(CreateFailedTitleResource),
+                            Content = string.Format(
+                                _resourceLoader.GetString(CreateFailedTextResource),
+                                dialog.PickedFileName,
+                                dialog.PickedFolder.DisplayName)
+                        };
+                        await saveFailedDialog.ShowAsync();
+                    }
+                }
+            }
+            while (true);
+        }
+    }
+}
+#endif
