62b70b573787ce67c1d661af824439f2d87433c1
Carl de Billy <carl.debilly@gmail.com>
2021-03-25 09:55:11 -0400

    Updated WebAuthBroker sample to latest version of Uno for AndroidX Chrome Custom Tabs.

--- UI/Authentication.OidcDemo/Authentication.OidcDemo/Authentication.OidcDemo.Droid/WebAuthenticationBrokerActivity.cs
+++ UI/Authentication.OidcDemo/Authentication.OidcDemo/Authentication.OidcDemo.Droid/WebAuthenticationBrokerActivity.cs
@@ -10,7 +10,7 @@ namespace Authentication.OidcDemo.Droid
 		Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
 		// Change the following DataScheme for one specific to your application...
 		DataScheme = "uno-wab-demo")]
-	public class WebAuthenticationBrokerActivity : WebAuthenticationBrokerActivityBase
+	public partial class WebAuthenticationBrokerActivity : Uno.AuthenticationBroker.WebAuthenticationBrokerActivityBase
 	{
 		// You can change the name of this class if you wish, it really not something important.
 	}
