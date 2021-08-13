177d16f714f0f72e3b2a1ddeebad42b7b748c2a8
Jérôme Laban <jlaban@gmail.com>
2021-08-13 09:01:47 -0400

    fix: fix SkiaSharp sample member visibility

--- UI/SkiaSharpTest/SkiaSharpTest/SkiaSharpTest.Shared/App.xaml.cs
+++ UI/SkiaSharpTest/SkiaSharpTest/SkiaSharpTest.Shared/App.xaml.cs
@@ -20,7 +20,7 @@ namespace SkiaSharpTest
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    public sealed partial class App : Application
     {
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
