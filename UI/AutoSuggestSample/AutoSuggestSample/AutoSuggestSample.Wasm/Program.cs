bbbd508970ab6930039b79662b743b9960b7929f
iury <100223422+iury-kc@users.noreply.github.com>
2022-04-05 07:57:00 -0300

    chore: AutoSuggestSample Uno 4.1.9

--- UI/AutoSuggestSample/AutoSuggestSample/AutoSuggestSample.Wasm/Program.cs
+++ UI/AutoSuggestSample/AutoSuggestSample/AutoSuggestSample.Wasm/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.Extensions.Logging;
-using Uno.Extensions;
+﻿using System; 
 using Windows.UI.Xaml;
 
 namespace AutoSuggestSample.Wasm
@@ -9,42 +7,11 @@ namespace AutoSuggestSample.Wasm
 	{
 		private static App _app;
 
-		static void Main(string[] args)
+		static int Main(string[] args)
 		{
-			ConfigureFilters(LogExtensionPoint.AmbientLoggerFactory);
+			Windows.UI.Xaml.Application.Start(_ => _app = new App());
 
-			Application.Start(_ => _app = new App());
-		}
-
-		static void ConfigureFilters(ILoggerFactory factory)
-		{
-			factory
-				.WithFilter(new FilterLoggerSettings
-					{
-						{ "Uno", LogLevel.Warning },
-						{ "Windows", LogLevel.Warning },
-						// { typeof(DependencyObjectStore).FullName, LogLevel.Debug },
-
-						// Generic Xaml events
-						// { "Windows.UI.Xaml", LogLevel.Debug },
-
-						// { "Uno.UI.Controls.AsyncValuePresenter", LogLevel.Debug },
-						// { "Uno.UI.Controls.IfDataContext", LogLevel.Debug },
-						   
-						// Layouter specific messages
-						// { "Windows.UI.Xaml.Controls", LogLevel.Debug },
-						//{ "Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug },
-						//{ "Windows.UI.Xaml.Controls.Panel", LogLevel.Debug },
-						   
-						// Binding related messages
-						// { "Windows.UI.Xaml.Data", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Data", LogLevel.Debug },
-						   
-						//  Binder memory references tracking
-						// { "ReferenceHolder", LogLevel.Debug },
-					}
-				)
-				.AddConsole(LogLevel.Debug);
+			return 0;
 		}
 	}
 }
