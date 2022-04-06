9b167cf42754ef042b519d779595357d5abebf69
iury <100223422+iury-kc@users.noreply.github.com>
2022-04-06 15:01:10 -0300

    chore: EthereumBlockChain

--- UI/UnoEthereumBlockChain/DemoApp/DemoApp/DemoApp.Shared/App.xaml.cs
+++ UI/UnoEthereumBlockChain/DemoApp/DemoApp/DemoApp.Shared/App.xaml.cs
@@ -182,6 +182,10 @@ namespace DemoApp
             });
 
             global::Uno.Extensions.LogExtensionPoint.AmbientLoggerFactory = factory;
+
+#if HAS_UNO
+			global::Uno.UI.Adapter.Microsoft.Extensions.Logging.LoggingAdapter.Initialize();
+#endif
         }
     }
 }
