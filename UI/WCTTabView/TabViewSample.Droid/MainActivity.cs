91f3bb376a1e0d9d36d1c18b05899a2577904190
esteves <aesteves900@gmail.com>
2022-10-25 20:28:03 -0300

    chore: TabView Sample - Update to net6
    chore: TabView Sample - Update to net6
    
    chore: adjusted styles

---
+++ UI/WCTTabView/TabViewSample.Droid/MainActivity.cs
@@ -0,0 +1,14 @@
+namespace TabViewSample.Droid
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
