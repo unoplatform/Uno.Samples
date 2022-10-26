eaea454af5a5e0f75f04a95d4ae2811e04cd0add
eriklimakc <erik.lima@knowcode.app>
2022-10-26 09:39:14 -0300

    test: Added some unit tests to SimpleCalculator

--- UI/SimpleCalculator/SimpleCalculator.Tests/AppInfoTests.cs
+++ UI/SimpleCalculator/SimpleCalculator.Tests/AppInfoTests.cs
@@ -1,5 +1,4 @@
-
-
+using SimpleCalculator.Business;
 
 namespace SimpleCalculator.Tests;
 
@@ -10,12 +9,92 @@ public class AppInfoTests
 	{
 	}
 
-	[Test]
-	public void AppInfoCreation()
-	{
-		var appInfo = new AppConfig { Title = "Test" };
+    [Test]
+    public void MultiplicationTest()
+    {
+		Calculator c = new();
+		c = c.Input("1")
+			 .Input("1")
+			 .Input("×")
+			 .Input("4")
+			 .Input("=");
 
-		appInfo.Should().NotBeNull();
-		appInfo.Title.Should().Be("Test");
-	}
+		Assert.AreEqual("44", c.OutPut);
+        Assert.AreEqual("11 × 4 =", c.Equation);
+    }
+
+    [Test]
+    public void DivisionTest()
+    {
+        Calculator c = new();
+        c = c.Input("6")
+             .Input("5")
+             .Input("6")
+             .Input("1")
+             .Input("÷")
+             .Input("9")
+             .Input("=");
+
+        Assert.AreEqual("729", c.OutPut);
+        Assert.AreEqual("6561 ÷ 9 =", c.Equation);
+    }
+
+    [Test]
+    public void AdditionTest()
+    {
+        Calculator c = new();
+        c = c.Input("1")
+             .Input("2")
+             .Input("9")
+             .Input("0")
+             .Input("+")
+             .Input("9")
+             .Input("5")
+             .Input("2")
+             .Input("1")
+             .Input("=");
+
+        Assert.AreEqual("10811", c.OutPut);
+        Assert.AreEqual("1290 + 9521 =", c.Equation);
+    }
+
+    [Test]
+    public void SubtractionTest()
+    {
+        Calculator c = new();
+        c = c.Input("9")
+             .Input("9")
+             .Input("9")
+             .Input("-")
+             .Input("1")
+             .Input("0")
+             .Input("0")
+             .Input("0")
+             .Input("=");
+
+        Assert.AreEqual("-1", c.OutPut);
+        Assert.AreEqual("999 - 1000 =", c.Equation);
+    }
+
+    [Test]
+    public void MultiOperationTest()
+    {
+        Calculator c = new();
+        c = c.Input("1")
+             .Input("+")
+             .Input("4")
+             .Input("=")
+             .Input("×")
+             .Input("2")
+             .Input("=")
+             .Input("-")
+             .Input("1")
+             .Input("=")
+             .Input("÷")
+             .Input("3")
+             .Input("=");
+
+        Assert.AreEqual("3", c.OutPut);
+        Assert.AreEqual("9 ÷ 3 =", c.Equation);
+    }
 }
