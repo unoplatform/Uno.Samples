760e81885134cd5fd2116363f3104c1f92edf246
eriklimakc <erik.lima@knowcode.app>
2022-10-28 18:18:11 -0300

    chore: Keyboard support

---
+++ UI/SimpleCalculator/SimpleCalculator/Keyboard/KeyboardBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Uno.Extensions.Reactive;
+
+namespace SimpleCalculator.Keyboard
+{
+    public static class KeyboardBehavior
+    {
+        #region KeyUpCommand (Attached DP)
+        public static readonly DependencyProperty KeyUpCommandProperty = DependencyProperty.RegisterAttached(
+            "KeyUpCommand", typeof(ICommand), typeof(UIElement), new PropertyMetadata(default(Command), OnKeyUpCommandChanged));
+        public static ICommand GetKeyUpCommand(UIElement element)
+            => (ICommand)element.GetValue(KeyUpCommandProperty);
+        public static void SetKeyUpCommand(UIElement element, ICommand command)
+            => element.SetValue(KeyUpCommandProperty, command);
+        private static void OnKeyUpCommandChanged(DependencyObject snd, DependencyPropertyChangedEventArgs args)
+        {
+            if (snd is UIElement elt)
+            {
+                elt.KeyUp -= OnKeyUp;
+                if (args.NewValue is ICommand)
+                {
+                    elt.KeyUp += OnKeyUp;
+                }
+            }
+        }
+        private static void OnKeyUp(object snd, KeyRoutedEventArgs e)
+        {
+            if (snd is UIElement elt && GetKeyUpCommand(elt) is { } command && command.CanExecute(e.Key.ToString()))
+            {
+                command.Execute(e.Key.ToString());
+            }
+        }
+        #endregion
+    }
+}
--- UI/SimpleCalculator/SimpleCalculator/Presentation/MainViewModel.cs
+++ UI/SimpleCalculator/SimpleCalculator/Presentation/MainViewModel.cs
@@ -1,6 +1,8 @@
 
 using SimpleCalculator.Business;
+using SimpleCalculator.Keyboard;
 using Uno.Extensions.Reactive;
+using Windows.System;
 
 namespace SimpleCalculator.Presentation;
 
@@ -10,6 +12,14 @@ public partial class MainViewModel
     public IState<Calculator> Calculator => State.Value(this, () => new Calculator());
     public async ValueTask Input(string key, CancellationToken ct)
             => await Calculator.Update(c => c?.Input(key), ct);
+
+	public async ValueTask KeyboardInput(string key, CancellationToken ct)
+	{
+		string? currentKey = KeyValues.Keys[key];
+
+        if(!string.IsNullOrEmpty(key))
+            await Calculator.Update(c => c?.Input(currentKey), ct);
+    }
 
     public MainViewModel(
 		INavigator navigator,
