6023e56232d8728d1263764e1e0750b0c0ae3b11
David <github.site@torick.net>
2022-11-01 14:06:51 -0400

    fix: Fix keyboard input

--- reference/SimpleCalculator/SimpleCalculator/Presentation/MainModel.cs
+++ reference/SimpleCalculator/SimpleCalculator/Presentation/MainModel.cs
@@ -8,18 +8,7 @@ namespace SimpleCalculator.Presentation;
 public partial class MainModel
 {
 	public IState<Calculator> Calculator => State.Value(this, () => new Calculator());
+
 	public async ValueTask Input(string key, CancellationToken ct)
 			=> await Calculator.Update(c => c?.Input(key), ct);
-
-	public async ValueTask KeyboardInput(string key, CancellationToken ct)
-	{
-		string? currentKey = KeyValues.Keys[key];
-
-		if(!string.IsNullOrEmpty(key))
-			await Calculator.Update(c => c?.Input(currentKey), ct);
-	}
-
-	public MainModel()
-	{ 
-	}
 }
