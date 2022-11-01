e9edd935bf1a14a71451361498f122c9ce76c6f5
David <github.site@torick.net>
2022-11-01 18:23:24 -0400

    fix: Fix merge  issue

--- reference/SimpleCalculator/SimpleCalculator.UI/Views/CalculatorMarkupView.cs
+++ reference/SimpleCalculator/SimpleCalculator.UI/Views/CalculatorMarkupView.cs
@@ -96,7 +96,7 @@ namespace SimpleCalculator.Views
 			);
 		}
 
-		private Button RenderButton(MainViewModel.BindableMainViewModel vm, int gridRow, int gridColumn, string content, string background, string foreground = "OnPrimaryBrush", string? parameter = null)
+		private Button RenderButton(MainModel.MainViewModel vm, int gridRow, int gridColumn, string content, string background, string foreground = "OnPrimaryBrush", string? parameter = null)
 			=> new Button()
 			.Command(() => vm.Input)
 			.CommandParameter(parameter ?? content)
