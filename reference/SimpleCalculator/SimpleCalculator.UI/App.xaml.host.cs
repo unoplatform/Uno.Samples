0049f202fc5f8eafb73d03135d51ec0fc9837626
eriklimakc <erik.lima@knowcode.app>
2022-11-02 15:07:09 -0300

    WIP - to keep working with corrent package

--- reference/SimpleCalculator/SimpleCalculator.UI/App.xaml.host.cs
+++ reference/SimpleCalculator/SimpleCalculator.UI/App.xaml.host.cs
@@ -66,7 +66,7 @@ public sealed partial class App : Application
 	{
 		views.Register(
 			new ViewMap( ViewModel: typeof(ShellViewModel)),
-			new ViewMap<CalculatorMarkupView, MainModel>()
+			new ViewMap<MainPageMarkup, MainModel>()
 			);
 
 		routes
