31a5538ade1b4caedf30d20d6b7272c0bf25e030
Peter Freeman Foot <peter@inthehand.com>
2022-11-06 18:31:43 +0000

    Completed sample with nested and separated versions of dynamic menu

---
+++ UI/InsertingSeparators/InsertingSeparators.Shared/MenuTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsertingSeparators
+{
+    public class MenuTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate NormalTemplate { get; set; }
+        public DataTemplate SeparatorTemplate { get; set; }
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            if(item is Bird)
+            {
+                return NormalTemplate;
+            }
+
+            return base.SelectTemplateCore(item);
+        }
+    }
+}
