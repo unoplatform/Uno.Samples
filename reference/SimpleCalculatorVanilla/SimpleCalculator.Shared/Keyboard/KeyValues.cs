b5ed8fd9b9f0b0b0ee98f071511631c9512125f3
eriklimakc <erik.lima@knowcode.app>
2022-11-10 12:02:34 -0300

    chore: adding keyboard support

---
+++ reference/SimpleCalculatorVanilla/SimpleCalculator.Shared/Keyboard/KeyValues.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculator.Keyboard
+{
+    public static class KeyValues
+    {
+        public static Dictionary<string, string> Keys = new()
+        {
+            { "Number0", "0" },
+            { "NumberPad0", "0" },
+            { "Number1", "1" },
+            { "NumberPad1", "1" },
+            { "Number2", "2" },
+            { "NumberPad2", "2" },
+            { "Number3", "3" },
+            { "NumberPad3", "3" },
+            { "Number4", "4" },
+            { "NumberPad4", "4" },
+            { "Number5", "5" },
+            { "NumberPad5", "5" },
+            { "Number6", "6" },
+            { "NumberPad6", "6" },
+            { "Number7", "7" },
+            { "NumberPad7", "7" },
+            { "Number8", "8" },
+            { "NumberPad8", "8" },
+            { "Number9", "9" },
+            { "NumberPad9", "9" },
+            { "Add", "+" },
+            { "Divide", "÷" },
+            { "Enter","=" },
+            { "Subtract","-" },
+            { "Multiply", "×" },
+            { "*", "×" },
+            { "X", "×" },
+            { "Back", "back" },
+            { "Delete", "back" },
+            { "C", "C" },
+            { "Decimal", "." }
+        };
+    }
+}
