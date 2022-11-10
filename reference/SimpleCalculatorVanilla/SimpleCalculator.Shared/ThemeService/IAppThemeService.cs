ab02da1603c91b592d32d13f5c81d28a436f3b4b
eriklimakc <erik.lima@knowcode.app>
2022-11-10 13:57:45 -0300

    wip - theme switching support

---
+++ reference/SimpleCalculatorVanilla/SimpleCalculator.Shared/ThemeService/IAppThemeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SimpleCalculator.ThemeService
+{
+    public interface IAppThemeService
+    {
+        bool IsDark { get; }
+        ValueTask SetThemeAsync(bool darkMode, CancellationToken ct);
+    }
+}
