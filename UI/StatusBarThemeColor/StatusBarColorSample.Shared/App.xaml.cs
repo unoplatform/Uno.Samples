36568e36fd12d83aa06c51c7dfd301330393ee19
esteves <aesteves900@gmail.com>
2022-11-18 19:22:53 -0300

    chore: StatusBarColor Sample - Update to net6
    chore: StatusBarColor Sample - Update to net6

--- UI/StatusBarThemeColor/StatusBarColorSample.Shared/App.xaml.cs
+++ UI/StatusBarThemeColor/StatusBarColorSample.Shared/App.xaml.cs
@@ -1,153 +1,168 @@
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.ViewManagement;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace StatusBarColorSample
 {
-	/// <summary>
-	/// Provides application-specific behavior to supplement the default Application class.
-	/// </summary>
-	public sealed partial class App : Application
-	{
-		private UISettings _uiSettings;
-		private bool? _wasDarkMode = null;
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public sealed partial class App : Application
+    {
+        private Window _window;
+        private UISettings _uiSettings;
+        private bool? _wasDarkMode = null;
 
-		/// <summary>
-		/// Initializes the singleton application object.  This is the first line of authored code
-		/// executed, and as such is the logical equivalent of main() or WinMain().
-		/// </summary>
-		public App()
-		{
 
-			InitializeLogging();
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeLogging();
+
+            this.InitializeComponent();
 
 #if __IOS__ || __ANDROID__
 			// Enable native frame navigation.
 			Uno.UI.FeatureConfiguration.Style.ConfigureNativeFrameNavigation();
 #endif
 
-			this.InitializeComponent();
-
 #if HAS_UNO || NETFX_CORE
-			this.Suspending += OnSuspending;
+            this.Suspending += OnSuspending;
 #endif
+        }
 
-		}
-
-		/// <summary>
-		/// Invoked when the application is launched normally by the end user.  Other entry points
-		/// will be used such as when the application is launched to open a specific file.
-		/// </summary>
-		/// <param name="e">Details about the launch request and process.</param>
-		protected override void OnLaunched(LaunchActivatedEventArgs e)
-		{
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
 #if DEBUG
-			if (System.Diagnostics.Debugger.IsAttached)
-			{
-				// this.DebugSettings.EnableFrameRateCounter = true;
-			}
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // this.DebugSettings.EnableFrameRateCounter = true;
+            }
 #endif
 
-#if NET5_0 && WINDOWS
-			var window = new Window();
-			window.Activate();
+#if NET6_0_OR_GREATER && WINDOWS && !HAS_UNO
+            _window = new Window();
+            _window.Activate();
 #else
-			var window = Windows.UI.Xaml.Window.Current;
+            _window = Microsoft.UI.Xaml.Window.Current;
 #endif
 
-			var rootFrame = window.Content as Frame;
+            var rootFrame = _window.Content as Frame;
 
-			// Do not repeat app initialization when the Window already has content,
-			// just ensure that the window is active
-			if (rootFrame == null)
-			{
-				// Create a Frame to act as the navigation context and navigate to the first page
-				rootFrame = new Frame();
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
 
-				rootFrame.NavigationFailed += OnNavigationFailed;
+                rootFrame.NavigationFailed += OnNavigationFailed;
 
-				if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-				{
-					//TODO: Load state from previously suspended application
-				}
+                if (args.UWPLaunchActivatedEventArgs.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    // TODO: Load state from previously suspended application
+                }
 
-				// Place the frame in the current Window
-				window.Content = rootFrame;
-			}
+                // Place the frame in the current Window
+                _window.Content = rootFrame;
+            }
 
-#if !(NET5_0 && WINDOWS)
-			if (e.PrelaunchActivated == false)
+#if !(NET6_0_OR_GREATER && WINDOWS)
+            if (args.UWPLaunchActivatedEventArgs.PrelaunchActivated == false)
 #endif
-			{
-				if (rootFrame.Content == null)
-				{
-					// When the navigation stack isn't restored navigate to the first page,
-					// configuring the new page by passing required information as a navigation
-					// parameter
-					rootFrame.Navigate(typeof(MainPage), e.Arguments);
-				}
-				// Ensure the current window is active
-				window.Activate();
-			}
+            {
+                if (rootFrame.Content == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                    rootFrame.Navigate(typeof(MainPage), args.Arguments);
+                }
+                // Ensure the current window is active
+                _window.Activate();
+            }
+        }
 
-			ConfigureStatusBar();
-		}
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new InvalidOperationException($"Failed to load {e.SourcePageType.FullName}: {e.Exception}");
+        }
 
-		private void ConfigureStatusBar()
-		{
-			// Listen for the system theme changes.
-			_uiSettings = new UISettings();
-			_uiSettings.ColorValuesChanged += (s, e) =>
-			{
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+
+
+        private void ConfigureStatusBar()
+        {
+            // Listen for the system theme changes.
+            _uiSettings = new UISettings();
+            _uiSettings.ColorValuesChanged += (s, e) =>
+            {
 #if __ANDROID__
 				var backgroundColor = _uiSettings.GetColorValue(UIColorType.Background);
-				var isDarkMode = backgroundColor == Windows.UI.Colors.Black;
+				var isDarkMode = backgroundColor == Microsoft.UI.Colors.Black;
 
 				// Prevent deadlock as setting StatusBar.ForegroundColor will also trigger this event.
 				if (_wasDarkMode == isDarkMode) return;
 				_wasDarkMode = isDarkMode;
 #endif
 
-				UpdateStatusBar();
-			};
+                UpdateStatusBar();
+            };
 
 #if __IOS__
 			// Force an update when the app is launched.
 			UpdateStatusBar();
 #endif
-		}
+        }
 
-		private void UpdateStatusBar()
-		{
-			// === 1. Determine the current theme from the background value, 
-			// which is calculated from the theme and can only be black or white.
-			var backgroundColor = _uiSettings.GetColorValue(UIColorType.Background);
-			var isDarkMode = backgroundColor == Windows.UI.Colors.Black;
+
+        private void UpdateStatusBar()
+        {
+            // === 1. Determine the current theme from the background value, 
+            // which is calculated from the theme and can only be black or white.
+            var backgroundColor = _uiSettings.GetColorValue(UIColorType.Background);
+            var isDarkMode = backgroundColor == Microsoft.UI.Colors.Black;
 
 #if __IOS__ || __ANDROID__
 			// === 2. Set the foreground color.
 			// note: The foreground color can only be set to a "dark/light" value. See uno remarks on StatusBar.ForegroundColor.
 			// note: For ios in dark mode, setting this value will have no effect.
-			var foreground = isDarkMode ? Windows.UI.Colors.White : Windows.UI.Colors.Black;
+			var foreground = isDarkMode ? Microsoft.UI.Colors.White : Microsoft.UI.Colors.Black;
 			Windows.UI.ViewManagement.StatusBar.GetForCurrentView().ForegroundColor = foreground;
 
 			// === 3. Set the background color.
-			var background = isDarkMode ? Windows.UI.Colors.MidnightBlue : Windows.UI.Colors.SkyBlue;
+			var background = isDarkMode ? Microsoft.UI.Colors.MidnightBlue : Microsoft.UI.Colors.SkyBlue;
 #if __ANDROID__
 			// On Android, this is done by calling Window.SetStatusBarColor.
 			if (Uno.UI.ContextHelper.Current is Android.App.Activity activity)
@@ -159,95 +174,79 @@ namespace StatusBarColorSample
 			// For android, we will also update the CommandBar just for consistency.
 			if (MainPage.Instance.GetCommandBar() is CommandBar commandBar)
 			{
-				commandBar.Foreground = new SolidColorBrush(foreground); // controls the color for the "MainPage" page title
-				commandBar.Background = new SolidColorBrush(background);
+				commandBar.Foreground = new Microsoft.UI.Xaml.Media.SolidColorBrush(foreground); // controls the color for the "MainPage" page title
+				commandBar.Background = new Microsoft.UI.Xaml.Media.SolidColorBrush(background);
 			}
 #endif
-		}
+        }
 
-		/// <summary>
-		/// Invoked when Navigation to a certain page fails
-		/// </summary>
-		/// <param name="sender">The Frame which failed navigation</param>
-		/// <param name="e">Details about the navigation failure</param>
-		void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-		{
-			throw new Exception($"Failed to load {e.SourcePageType.FullName}: {e.Exception}");
-		}
+        /// <summary>
+        /// Configures global Uno Platform logging
+        /// </summary>
+        private static void InitializeLogging()
+        {   
+#if DEBUG
+            // Logging is disabled by default for release builds, as it incurs a significant
+            // initialization cost from Microsoft.Extensions.Logging setup. If startup performance
+            // is a concern for your application, keep this disabled. If you're running on web or 
+            // desktop targets, you can use url or command line parameters to enable it.
+            //
+            // For more performance documentation: https://platform.uno/docs/articles/Uno-UI-Performance.html
 
-		/// <summary>
-		/// Invoked when application execution is being suspended.  Application state is saved
-		/// without knowing whether the application will be terminated or resumed with the contents
-		/// of memory still intact.
-		/// </summary>
-		/// <param name="sender">The source of the suspend request.</param>
-		/// <param name="e">Details about the suspend request.</param>
-		private void OnSuspending(object sender, SuspendingEventArgs e)
-		{
-			var deferral = e.SuspendingOperation.GetDeferral();
-			//TODO: Save application state and stop any background activity
-			deferral.Complete();
-		}
-
-
-		/// <summary>
-		/// Configures global Uno Platform logging
-		/// </summary>
-		private static void InitializeLogging()
-		{
-			var factory = LoggerFactory.Create(builder =>
-			{
+            var factory = LoggerFactory.Create(builder =>
+            {
 #if __WASM__
                 builder.AddProvider(new global::Uno.Extensions.Logging.WebAssembly.WebAssemblyConsoleLoggerProvider());
 #elif __IOS__
                 builder.AddProvider(new global::Uno.Extensions.Logging.OSLogLoggerProvider());
 #elif NETFX_CORE
-				builder.AddDebug();
+                builder.AddDebug();
 #else
                 builder.AddConsole();
 #endif
 
-				// Exclude logs below this level
-				builder.SetMinimumLevel(LogLevel.Information);
+                // Exclude logs below this level
+                builder.SetMinimumLevel(LogLevel.Information);
 
-				// Default filters for Uno Platform namespaces
-				builder.AddFilter("Uno", LogLevel.Warning);
-				builder.AddFilter("Windows", LogLevel.Warning);
-				builder.AddFilter("Microsoft", LogLevel.Warning);
+                // Default filters for Uno Platform namespaces
+                builder.AddFilter("Uno", LogLevel.Warning);
+                builder.AddFilter("Windows", LogLevel.Warning);
+                builder.AddFilter("Microsoft", LogLevel.Warning);
 
-				// Generic Xaml events
-				// builder.AddFilter("Windows.UI.Xaml", LogLevel.Debug );
-				// builder.AddFilter("Windows.UI.Xaml.VisualStateGroup", LogLevel.Debug );
-				// builder.AddFilter("Windows.UI.Xaml.StateTriggerBase", LogLevel.Debug );
-				// builder.AddFilter("Windows.UI.Xaml.UIElement", LogLevel.Debug );
-				// builder.AddFilter("Windows.UI.Xaml.FrameworkElement", LogLevel.Trace );
+                // Generic Xaml events
+                // builder.AddFilter("Windows.UI.Xaml", LogLevel.Debug );
+                // builder.AddFilter("Windows.UI.Xaml.VisualStateGroup", LogLevel.Debug );
+                // builder.AddFilter("Windows.UI.Xaml.StateTriggerBase", LogLevel.Debug );
+                // builder.AddFilter("Windows.UI.Xaml.UIElement", LogLevel.Debug );
+                // builder.AddFilter("Windows.UI.Xaml.FrameworkElement", LogLevel.Trace );
 
-				// Layouter specific messages
-				// builder.AddFilter("Windows.UI.Xaml.Controls", LogLevel.Debug );
-				// builder.AddFilter("Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug );
-				// builder.AddFilter("Windows.UI.Xaml.Controls.Panel", LogLevel.Debug );
+                // Layouter specific messages
+                // builder.AddFilter("Windows.UI.Xaml.Controls", LogLevel.Debug );
+                // builder.AddFilter("Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug );
+                // builder.AddFilter("Windows.UI.Xaml.Controls.Panel", LogLevel.Debug );
 
-				// builder.AddFilter("Windows.Storage", LogLevel.Debug );
+                // builder.AddFilter("Windows.Storage", LogLevel.Debug );
 
-				// Binding related messages
-				// builder.AddFilter("Windows.UI.Xaml.Data", LogLevel.Debug );
-				// builder.AddFilter("Windows.UI.Xaml.Data", LogLevel.Debug );
+                // Binding related messages
+                // builder.AddFilter("Windows.UI.Xaml.Data", LogLevel.Debug );
+                // builder.AddFilter("Windows.UI.Xaml.Data", LogLevel.Debug );
 
-				// Binder memory references tracking
-				// builder.AddFilter("Uno.UI.DataBinding.BinderReferenceHolder", LogLevel.Debug );
+                // Binder memory references tracking
+                // builder.AddFilter("Uno.UI.DataBinding.BinderReferenceHolder", LogLevel.Debug );
 
-				// RemoteControl and HotReload related
-				// builder.AddFilter("Uno.UI.RemoteControl", LogLevel.Information);
+                // RemoteControl and HotReload related
+                // builder.AddFilter("Uno.UI.RemoteControl", LogLevel.Information);
 
-				// Debug JS interop
-				// builder.AddFilter("Uno.Foundation.WebAssemblyRuntime", LogLevel.Debug );
-			});
+                // Debug JS interop
+                // builder.AddFilter("Uno.Foundation.WebAssemblyRuntime", LogLevel.Debug );
+            });
 
-			global::Uno.Extensions.LogExtensionPoint.AmbientLoggerFactory = factory;
+            global::Uno.Extensions.LogExtensionPoint.AmbientLoggerFactory = factory;
 
 #if HAS_UNO
-			global::Uno.UI.Adapter.Microsoft.Extensions.Logging.LoggingAdapter.Initialize();
+            global::Uno.UI.Adapter.Microsoft.Extensions.Logging.LoggingAdapter.Initialize();
 #endif
-		}
-	}
+#endif
+        }
+    }
 }
