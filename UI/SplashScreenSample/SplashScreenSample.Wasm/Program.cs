e9330210ea373e99fc794bc7ef417638c92cf846
esteves <aesteves900@gmail.com>
2022-11-21 17:51:51 -0300

    chore: SplashScreen Sample - Update to net6

--- UI/SplashScreenSample/SplashScreenSample.Wasm/Program.cs
+++ UI/SplashScreenSample/SplashScreenSample.Wasm/Program.cs
@@ -1,17 +1,17 @@
+﻿using Microsoft.UI.Xaml;
 using System;
-using Windows.UI.Xaml;
 
 namespace SplashScreenSample.Wasm
 {
-	public class Program
-	{
-		private static App _app;
+    public class Program
+    {
+        private static App _app;
 
-		static int Main(string[] args)
-		{
-			Windows.UI.Xaml.Application.Start(_ => _app = new App());
+        static int Main(string[] args)
+        {
+            Microsoft.UI.Xaml.Application.Start(_ => _app = new App());
 
-			return 0;
-		}
-	}
+            return 0;
+        }
+    }
 }
