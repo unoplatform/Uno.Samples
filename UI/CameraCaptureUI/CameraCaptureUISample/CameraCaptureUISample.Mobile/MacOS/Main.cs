adbb78594046e48f241ed63d121d02c7e7402c95
esteves <aesteves900@gmail.com>
2022-11-21 22:10:07 -0300

    chore: CameraCaptureUI Sample - Update to net6

---
+++ UI/CameraCaptureUI/CameraCaptureUISample/CameraCaptureUISample.Mobile/MacOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace CameraCaptureUISample.macOS
+{
+    internal static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new App();
+            NSApplication.Main(args);
+        }
+    }
+}
+
