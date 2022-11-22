9417d81ea0ea0732f9db679bbc62cffacb7b34fd
LeomarisReyes <reyes.leomaris@gmail.com>
2022-11-22 19:23:41 -0400

    Let's replicate a Food Delivery UI with Uno Platform

---
+++ UI/FoodDeliveryUI/FoodDeliveryUI/FoodDeliveryUI.Shared/Food.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FoodDeliveryUI
+{
+    public class Food
+    {
+        public string Picture { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } 
+    }
+}
