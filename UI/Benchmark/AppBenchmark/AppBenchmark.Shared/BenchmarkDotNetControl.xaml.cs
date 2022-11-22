607d1615090fe1d8b66507bc49d1c2a4acf26de1
esteves <aesteves900@gmail.com>
2022-11-21 22:02:57 -0300

    dev/AEST/190-update-to-net6-appbench

--- UI/Benchmark/AppBenchmark/AppBenchmark.Shared/BenchmarkDotNetControl.xaml.cs
+++ UI/Benchmark/AppBenchmark/AppBenchmark.Shared/BenchmarkDotNetControl.xaml.cs
@@ -1,157 +1,169 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Loggers;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Toolchains.InProcess;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+using BenchmarkDotNet.Toolchains;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.UI;
+using System.Reflection;
+using System.Threading.Tasks;
 using Windows.UI.Core;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Documents;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace AppBenchmark
 {
-	public sealed partial class BenchmarkDotNetControl : UserControl
-	{
-		private const string BenchmarksBaseNamespace = "SamplesApp.Benchmarks.Suite";
-		private TextBlockLogger _logger;
+    public sealed partial class BenchmarkDotNetControl : UserControl
+    {
+        private const string BenchmarksBaseNamespace = "SamplesApp.Benchmarks.Suite";
+        private TextBlockLogger _logger;
 
-		public BenchmarkDotNetControl()
-		{
-			this.InitializeComponent();
-		}
+        public BenchmarkDotNetControl()
+        {
+            this.InitializeComponent();
+        }
 
-		public string ClassFilter { get; set; } = "";
+        public string ClassFilter { get; set; } = "";
 
-		private void OnRunTests(object sender, object args)
-		{
-			_ = Dispatcher.RunAsync(
-				CoreDispatcherPriority.Normal,
-				async () => await Run()
-			);
-		}
+        private void OnRunTests(object sender, object args)
+        {
+            _ = DispatcherQueue.TryEnqueue(async () =>
+            {
+                await Run();
+            });
+        }
 
-		private async Task Run()
-		{
-			_logger = new TextBlockLogger(runLogs);
+        private async Task Run()
+        {
+            _logger = new TextBlockLogger(runLogs);
 
-			try
-			{
-				var config = new CoreConfig(_logger);
+            try
+            {
+                var config = new CoreConfig(_logger);
 
-				await SetStatus("Discovering benchmarks in " + BenchmarksBaseNamespace);
-				var types = EnumerateBenchmarks(config).ToArray();
+                await SetStatus("Discovering benchmarks in " + BenchmarksBaseNamespace);
+                var types = EnumerateBenchmarks(config).ToArray();
 
-				foreach (var type in types)
-				{
-					await SetStatus($"Running benchmarks for {type}");
-					var b = BenchmarkRunner.Run(type, config);
-				}
+                foreach (var type in types)
+                {
+                    await SetStatus($"Running benchmarks for {type}");
+                    var b = BenchmarkRunner.Run(type, config);
+                }
 
-				await SetStatus($"Done.");
-			}
-			catch(Exception e)
-			{
-				await SetStatus($"Failed {e?.Message}");
-				_logger.WriteLine(LogKind.Error, e?.ToString());
-			}
-		}
+                await SetStatus($"Done.");
+            }
+            catch (Exception e)
+            {
+                await SetStatus($"Failed {e?.Message}");
+                _logger.WriteLine(LogKind.Error, e?.ToString());
+            }
+        }
 
-		private async Task SetStatus(string status)
-		{
-			runStatus.Text = status;
-			await Task.Yield();
-		}
+        private async Task SetStatus(string status)
+        {
+            runStatus.Text = status;
+            await Task.Yield();
+        }
 
-		private IEnumerable<Type> EnumerateBenchmarks(IConfig config)
-			=> from type in GetType().GetTypeInfo().Assembly.GetTypes()
-			   where !type.IsGenericType
-			   where type.Namespace?.StartsWith(BenchmarksBaseNamespace) ?? false
-			   where BenchmarkConverter.TypeToBenchmarks(type, config).BenchmarksCases.Length != 0
-			   where string.IsNullOrEmpty(ClassFilter) || type.Name.Contains(ClassFilter)
-			   select type;
+        private IEnumerable<Type> EnumerateBenchmarks(IConfig config)
+            => from type in GetType().GetTypeInfo().Assembly.GetTypes()
+               where !type.IsGenericType
+               where type.Namespace?.StartsWith(BenchmarksBaseNamespace) ?? false
+               where BenchmarkConverter.TypeToBenchmarks(type, config).BenchmarksCases.Length != 0
+               where string.IsNullOrEmpty(ClassFilter) || type.Name.Contains(ClassFilter)
+               select type;
 
-		public class CoreConfig : ManualConfig
-		{
-			public CoreConfig(ILogger logger)
-			{
-				Add(logger);
+        public class CoreConfig : ManualConfig
+        {
+            public CoreConfig(ILogger logger)
+            {
+                AddLogger(logger);
 
 #if __WASM__
-				Add(AsciiDocExporter.Default);
+                AddExporter(AsciiDocExporter.Default);
 #endif
 
-				Add(Job.InProcess
-					.WithLaunchCount(1)
-					.WithWarmupCount(1)
-					.WithIterationCount(5)
-					.With(InProcessToolchain.Synchronous)
-					.WithId("InProcess")
-				);
+                AddJob(
+                    Job.InProcess
+                    .WithLaunchCount(1)
+                    .WithWarmupCount(1)
+                    .WithIterationCount(5)
+                    .WithToolchain(InProcessEmitToolchain.Instance)
+                    .WithId("InProcess")
+                );
 
-				ArtifactsPath = Path.GetTempPath();
-			}
-		}
+                ArtifactsPath = Path.GetTempPath();
+            }
+        }
 
-		private class TextBlockLogger : ILogger
-		{
-			private static Dictionary<LogKind, SolidColorBrush> ColorfulScheme { get; } =
-			   new Dictionary<LogKind, SolidColorBrush>
-			   {
-					{ LogKind.Default, new SolidColorBrush(Colors.Gray) },
-					{ LogKind.Help, new SolidColorBrush(Colors.DarkGreen) },
-					{ LogKind.Header, new SolidColorBrush(Colors.Magenta) },
-					{ LogKind.Result, new SolidColorBrush(Colors.DarkCyan) },
-					{ LogKind.Statistic, new SolidColorBrush(Colors.Cyan) },
-					{ LogKind.Info, new SolidColorBrush(Colors.DarkOrange) },
-					{ LogKind.Error, new SolidColorBrush(Colors.Red) },
-					{ LogKind.Hint, new SolidColorBrush(Colors.DarkCyan) }
-			   };
+        private class TextBlockLogger : ILogger
+        {
+            private static Dictionary<LogKind, SolidColorBrush> ColorfulScheme { get; } =
+               new Dictionary<LogKind, SolidColorBrush>
+               {
+                    { LogKind.Default, new SolidColorBrush(Colors.Gray) },
+                    { LogKind.Help, new SolidColorBrush(Colors.DarkGreen) },
+                    { LogKind.Header, new SolidColorBrush(Colors.Magenta) },
+                    { LogKind.Result, new SolidColorBrush(Colors.DarkCyan) },
+                    { LogKind.Statistic, new SolidColorBrush(Colors.Cyan) },
+                    { LogKind.Info, new SolidColorBrush(Colors.DarkOrange) },
+                    { LogKind.Error, new SolidColorBrush(Colors.Red) },
+                    { LogKind.Hint, new SolidColorBrush(Colors.DarkCyan) }
+               };
 
-			private readonly TextBlock _target;
+            string ILogger.Id => "TextBlockLogger";
 
-			public TextBlockLogger(TextBlock target)
-				=> _target = target;
+            int ILogger.Priority => 1;
 
-			public void Flush() { }
+            private readonly TextBlock _target;
 
-			public void Write(LogKind logKind, string text)
-			{
-				_target.Inlines.Add(new Run { Text = text, Foreground = GetLogKindColor(logKind) });
-			}
+            public TextBlockLogger(TextBlock target)
+                => _target = target;
 
-			public static Brush GetLogKindColor(LogKind logKind)
-			{
-				if (!ColorfulScheme.TryGetValue(logKind, out var brush))
-				{
-					brush = ColorfulScheme[LogKind.Default];
-				}
+            public void Flush() { }
 
-				return brush;
-			}
+            public void Write(LogKind logKind, string text)
+            {
+                _target.Inlines.Add(new Run { Text = text, Foreground = GetLogKindColor(logKind) });
+            }
 
-			public void WriteLine() => _target.Inlines.Add(new LineBreak());
+            public static Brush GetLogKindColor(LogKind logKind)
+            {
+                if (!ColorfulScheme.TryGetValue(logKind, out var brush))
+                {
+                    brush = ColorfulScheme[LogKind.Default];
+                }
 
-			public void WriteLine(LogKind logKind, string text)
-			{
-				Write(logKind, text);
-				WriteLine();
-			}
-		}
-	}
+                return brush;
+            }
+
+            public void WriteLine() => _target.Inlines.Add(new LineBreak());
+
+            public void WriteLine(LogKind logKind, string text)
+            {
+                Write(logKind, text);
+                WriteLine();
+            }
+        }
+
+    }
 }
