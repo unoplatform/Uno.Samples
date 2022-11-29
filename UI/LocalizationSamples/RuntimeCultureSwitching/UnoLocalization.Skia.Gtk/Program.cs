e8643c1e5f39de5d43c38ddae36e3bc79f45b82e
esteves <aesteves900@gmail.com>
2022-11-29 16:44:44 -0300

    chore: Localization Sample - Update to net6 - solution2
    chore: Localization Sample - Update to net6 - solution2

--- UI/LocalizationSamples/RuntimeCultureSwitching/UnoLocalization.Skia.Gtk/Program.cs
+++ UI/LocalizationSamples/RuntimeCultureSwitching/UnoLocalization.Skia.Gtk/Program.cs
@@ -1,22 +1,22 @@
+﻿using GLib;
 using System;
-using GLib;
 using Uno.UI.Runtime.Skia;
 
 namespace UnoLocalization.Skia.Gtk
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			ExceptionManager.UnhandledException += delegate (UnhandledExceptionArgs expArgs)
-			{
-				Console.WriteLine("GLIB UNHANDLED EXCEPTION" + expArgs.ExceptionObject.ToString());
-				expArgs.ExitApplication = true;
-			};
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExceptionManager.UnhandledException += delegate (UnhandledExceptionArgs expArgs)
+            {
+                Console.WriteLine("GLIB UNHANDLED EXCEPTION" + expArgs.ExceptionObject.ToString());
+                expArgs.ExitApplication = true;
+            };
 
-			var host = new GtkHost(() => new App(), args);
+            var host = new GtkHost(() => new App());
 
-			host.Run();
-		}
-	}
+            host.Run();
+        }
+    }
 }
