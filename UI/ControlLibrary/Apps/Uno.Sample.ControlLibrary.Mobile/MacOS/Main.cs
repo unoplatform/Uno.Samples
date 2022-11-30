95d49c0542b49ace342d9339c9ab6a281a6de93d
esteves <aesteves900@gmail.com>
2022-11-30 15:11:28 -0300

    chore: ControlLibrary Sample - Update to net6

---
+++ UI/ControlLibrary/Apps/Uno.Sample.ControlLibrary.Mobile/MacOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace Uno.Sample.ControlLibrary.macOS
+{
+    internal static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new App();
+            NSApplication.Main(args);
+        }
+    }
+}
+
---
+++ UI/ControlLibrary/Apps/Uno.Sample.ControlLibrary.Shared/MyTemplatedControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace XamlControlLibrary
+{
+    public partial class MyTemplatedControl : Control
+    {
+		public MyTemplatedControl()
+		{
+			DefaultStyleKey = typeof(MyTemplatedControl);
+		}
+
+		public int MyProperty
+		{
+			get { return (int)GetValue(MyPropertyProperty); }
+			set { SetValue(MyPropertyProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty MyPropertyProperty =
+			DependencyProperty.Register(
+				"MyProperty",
+				typeof(int), 
+				typeof(MyTemplatedControl),
+				new PropertyMetadata(
+					0, 
+					(s, e) => ((MyTemplatedControl)s)?.OnMyPropertyChanged(e))
+			);
+
+
+		private void OnMyPropertyChanged(DependencyPropertyChangedEventArgs e)
+		{
+			// System.Diagnostics.Debug.WriteLine($"MyProperty Changed from [{e.OldValue}] to [{e.NewValue}]");
+		}
+	}
+}
