c3435714eea47283c210058806c6b9fab135dbd7
esteves <aesteves900@gmail.com>
2022-11-30 10:28:01 -0300

    chore: EmbeddedResources Sample - Update to net6

--- UI/EmbeddedResources/EmbeddedResourcesSample/EmbeddedResourcesSample.Shared/App.xaml.cs
+++ UI/EmbeddedResources/EmbeddedResourcesSample/EmbeddedResourcesSample.Shared/App.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace EmbeddedResourcesSample
 {
@@ -45,7 +35,7 @@ namespace EmbeddedResourcesSample
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
         /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
@@ -54,11 +44,11 @@ namespace EmbeddedResourcesSample
             }
 #endif
 
-#if NET5_0 && WINDOWS
+#if NET6_0_OR_GREATER && WINDOWS && !HAS_UNO
             _window = new Window();
             _window.Activate();
 #else
-            _window = Windows.UI.Xaml.Window.Current;
+            _window = Microsoft.UI.Xaml.Window.Current;
 #endif
 
             var rootFrame = _window.Content as Frame;
@@ -72,7 +62,7 @@ namespace EmbeddedResourcesSample
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
-                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                if (args.UWPLaunchActivatedEventArgs.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
                     // TODO: Load state from previously suspended application
                 }
@@ -81,8 +71,8 @@ namespace EmbeddedResourcesSample
                 _window.Content = rootFrame;
             }
 
-#if !(NET5_0 && WINDOWS)
-            if (args.PrelaunchActivated == false)
+#if !(NET6_0_OR_GREATER && WINDOWS)
+            if (args.UWPLaunchActivatedEventArgs.PrelaunchActivated == false)
 #endif
             {
                 if (rootFrame.Content == null)
@@ -117,7 +107,7 @@ namespace EmbeddedResourcesSample
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
-            // TODO: Save application state and stop any background activity
+            //TODO: Save application state and stop any background activity
             deferral.Complete();
         }
 
@@ -126,6 +116,14 @@ namespace EmbeddedResourcesSample
         /// </summary>
         private static void InitializeLogging()
         {
+#if DEBUG
+            // Logging is disabled by default for release builds, as it incurs a significant
+            // initialization cost from Microsoft.Extensions.Logging setup. If startup performance
+            // is a concern for your application, keep this disabled. If you're running on web or 
+            // desktop targets, you can use url or command line parameters to enable it.
+            //
+            // For more performance documentation: https://platform.uno/docs/articles/Uno-UI-Performance.html
+
             var factory = LoggerFactory.Create(builder =>
             {
 #if __WASM__
@@ -178,6 +176,7 @@ namespace EmbeddedResourcesSample
 
 #if HAS_UNO
             global::Uno.UI.Adapter.Microsoft.Extensions.Logging.LoggingAdapter.Initialize();
+#endif
 #endif
         }
     }
