3f12229deedf1cdd91f7a73d6290108d1e9c9ed5
Jerome Laban <jerome.laban@nventive.com>
2022-11-30 22:55:38 -0500

    feat: Add net7 EFCore+SQLite sample

---
+++ UI/EFCoreSQLiteSample/EFCoreSQLiteSample/EFCoreSQLiteSample.Shared/DataProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Threading;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EFCoreSQLiteSample
+{
+    public class DataProvider
+    {
+        static DataProvider()
+        {
+#if __WASM__
+            SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_sqlite3());
+#endif
+        }
+
+        public static async Task<string[]> Run(string entry)
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Database.EnsureCreated();
+
+                Console.WriteLine("Database created");
+
+                db.Blogs.Add(new Blog { Url = entry });
+                var count = await db.SaveChangesAsync(CancellationToken.None);
+
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(" - {0}", blog.Url);
+                }
+
+                return db.Blogs.Select(b => b.Url).ToArray();
+            }
+        }
+    }
+}
