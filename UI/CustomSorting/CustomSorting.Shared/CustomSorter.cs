948b9337996a0a8827fef7be31acfbedd85e62bb
Peter Freeman Foot <peter@inthehand.com>
2022-12-07 10:42:14 +0000

    Custom Sorting sample code

---
+++ UI/CustomSorting/CustomSorting.Shared/CustomSorter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace CustomSorting
+{
+    public class CustomSorter : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            int result = GetSurnameFromDisplayName(x.DisplayName).CompareTo(GetSurnameFromDisplayName(y.DisplayName));
+            //Debug.WriteLine($"{x.DisplayName} - {y.DisplayName} = {result}");
+            return result;
+        }
+
+        private static string GetSurnameFromDisplayName(string displayName)
+        {
+            if(displayName.Contains(','))
+            {
+                //surname first
+                var parts = displayName.Split(',');
+                if (parts.Length > 0)
+                    return parts[0].Trim();
+            }
+            else
+            {
+                //surname last
+                var parts = displayName.Split(' ');
+                //return last name
+                return parts[parts.Length - 1].Trim();
+            }
+
+            return string.Empty;
+        }
+    }
+}
