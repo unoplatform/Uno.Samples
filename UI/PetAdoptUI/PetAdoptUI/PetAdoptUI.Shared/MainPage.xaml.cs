6f98c5ebe1c90d7ee2a933550ae2df499f83a8b6
AndresAldana <andres.aldana@horus.com.uy>
2022-12-13 12:36:52 -0500

    feat: multiple images

--- UI/PetAdoptUI/PetAdoptUI/PetAdoptUI.Shared/MainPage.xaml.cs
+++ UI/PetAdoptUI/PetAdoptUI/PetAdoptUI.Shared/MainPage.xaml.cs
@@ -1,30 +1,28 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+﻿using Microsoft.UI.Xaml.Controls;
+using System.Collections.ObjectModel;
 
 namespace PetAdoptUI
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        public readonly ObservableCollection<string> PetImages = new ObservableCollection<string>();
+
         public MainPage()
         {
             this.InitializeComponent();
+            GenerateImages();
+        }
+
+        private void GenerateImages()
+        {
+            PetImages.Add("https://picsum.photos/id/237/390/360");
+            PetImages.Add("https://picsum.photos/id/40/390/360");
+            PetImages.Add("https://picsum.photos/id/582/390/360");
+            PetImages.Add("https://picsum.photos/id/593/390/360");
+            PetImages.Add("https://picsum.photos/id/659/390/360");
+            PetImages.Add("https://picsum.photos/id/718/390/360");
+            PetImages.Add("https://picsum.photos/id/783/390/360");
+
         }
     }
 }
