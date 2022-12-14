bf5aad38275ca0a631cbd205f3897166f634214e
esteves <aesteves900@gmail.com>
2022-12-13 23:29:26 -0300

    chore: SkiaSharpTest Sample - Update to net6

--- UI/SkiaSharpTest/SkiaSharpTest/SkiaSharpTest.Skia.Linux.FrameBuffer/Program.cs
+++ UI/SkiaSharpTest/SkiaSharpTest/SkiaSharpTest.Skia.Linux.FrameBuffer/Program.cs
@@ -1,28 +1,41 @@
-using SkiaSharp.Views.UWP;
+﻿using Microsoft.UI.Xaml;
 using System;
 using Uno.UI.Runtime.Skia;
+using Windows.UI.Core;
 
 namespace SkiaSharpTest
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			try
-			{
-				Console.CursorVisible = false;
-				
-				// SKSwapChain Panel is not supported inside of Uno yet.
-				SKSwapChainPanel.RaiseOnUnsupported = false;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.CursorVisible = false;
 
+                var host = new FrameBufferHost(() =>
+                {
+                    // Framebuffer applications don't have a WindowManager to rely
+                    // on. To close the application, we can hook onto CoreWindow events
+                    // which dispatch keyboard input, and close the application as a result.
+                    // This block can be moved to App.xaml.cs if it does not interfere with other
+                    // platforms that may use the same keys.
+                    CoreWindow.GetForCurrentThread().KeyDown += (s, e) =>
+                    {
+                        if (e.VirtualKey == Windows.System.VirtualKey.F12)
+                        {
+                            Application.Current.Exit();
+                        }
+                    };
 
-				var host = new FrameBufferHost(() => new App(), args);
-				host.Run();
-			}
-			finally
-			{
-				Console.CursorVisible = true;
-			}
-		}
-	}
+                    return new App();
+                });
+                host.Run();
+            }
+            finally
+            {
+                Console.CursorVisible = true;
+            }
+        }
+    }
 }
