5eb89ab2d13f4db71db6f5eac57dfc69eeddfa9a
esteves <aesteves900@gmail.com>
2022-12-22 09:56:45 -0300

    chore: MapControl Sample - Update to net6

--- UI/MapControlSample/MapControlSample/MapControlSample.Shared/MainPage.xaml.cs
+++ UI/MapControlSample/MapControlSample/MapControlSample.Shared/MainPage.xaml.cs
@@ -6,28 +6,28 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace MapControlSample
 {
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
-	public sealed partial class MainPage : Page
-	{
-		public MainPage()
-		{
-			this.InitializeComponent();
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
 
-			MapControl.Center = new Geopoint(new BasicGeoposition { Latitude = 45.491912, Longitude = -73.551542 });
-		}
-	}
+            MapControl.Center = new Geopoint(new BasicGeoposition { Latitude = 45.491912, Longitude = -73.551542 });
+        }
+    }
 }
