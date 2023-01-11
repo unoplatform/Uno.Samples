ad9c122700dc1972a8a828c46a2073ebd741d71c
eriklimakc <erik.lima@knowcode.app>
2023-01-11 13:15:39 +0000

    chore: VirtualKeys and KeyInputs on Test Project

--- reference/SimpleCalculator/SimpleCalculator.Tests/AppInfoTests.cs
+++ reference/SimpleCalculator/SimpleCalculator.Tests/AppInfoTests.cs
@@ -1,20 +1,20 @@
-using SimpleCalculator.Business;
+ï»¿using SimpleCalculator.Business;
 
 namespace SimpleCalculator.Tests;
 
 public class AppInfoTests
 {
-    [TestCase("11×4 =", "44", "11 × 4 =")]
-    [TestCase("6561÷9=", "729", "6561 ÷ 9 =")]
-    [TestCase("1290+9521=", "10811", "1290 + 9521 =")]
-    [TestCase("999-1000=", "-1", "999 - 1000 =")]
-    [TestCase("1+4=×2=-1=÷3=", "3", "9 ÷ 3 =")]
-    public void OperationTest(string input, string output, string equation)
+    [TestCase(new KeyInput[] { KeyInput.One, KeyInput.One, KeyInput.Multiplication, KeyInput.Four, KeyInput.Equal }, "44", "11 Ã— 4 =")]
+    [TestCase(new KeyInput[] { KeyInput.Six, KeyInput.Five, KeyInput.Six, KeyInput.One, KeyInput.Division, KeyInput.Nine, KeyInput.Equal }, "729", "6561 Ã· 9 =")]
+    [TestCase(new KeyInput[] { KeyInput.One, KeyInput.Two, KeyInput.Nine, KeyInput.Zero, KeyInput.Addition, KeyInput.Nine, KeyInput.Five, KeyInput.Two, KeyInput.One, KeyInput.Equal }, "10811", "1290 + 9521 =")]
+    [TestCase(new KeyInput[] { KeyInput.Nine, KeyInput.Nine, KeyInput.Nine, KeyInput.Subtraction, KeyInput.One, KeyInput.Zero, KeyInput.Zero, KeyInput.Zero, KeyInput.Equal }, "-1", "999 âˆ’ 1000 =")]
+    [TestCase(new KeyInput[] { KeyInput.One, KeyInput.Addition, KeyInput.Four, KeyInput.Equal, KeyInput.Multiplication, KeyInput.Two, KeyInput.Equal, KeyInput.Subtraction, KeyInput.One, KeyInput.Equal, KeyInput.Division, KeyInput.Three, KeyInput.Equal }, "3", "9 Ã· 3 =")]
+    public void OperationTest(KeyInput[] input, string output, string equation)
     {
         Calculator c = new();
 
         foreach (var value in input)
-            c = c.Input(value.ToString());
+            c = c.Input(value);
 
         Assert.AreEqual(output, c.Output);
         Assert.AreEqual(equation, c.Equation);
