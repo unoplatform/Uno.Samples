7e0f0b8ec97be27b8043dd39104c3ae783e38c71
eriklimakc <erik.lima@knowcode.app>
2023-01-12 15:38:02 +0000

    chore: Updating IState properties to use the same instance

--- reference/SimpleCalc/SimpleCalculator/Presentation/MainModel.cs
+++ reference/SimpleCalc/SimpleCalculator/Presentation/MainModel.cs
@@ -1,9 +1,4 @@
 ﻿using SimpleCalculator.Business;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
 using Uno.Extensions.Reactive;
 using SimpleCalculator.ThemeService;
 using Windows.System;
@@ -12,9 +7,10 @@ namespace SimpleCalculator.Presentation;
 
 public partial record MainModel
 {
-    public IState<bool> IsDark => State.Value(this, () => _theme.IsDark);
+    public IState<bool> IsDark { get; }
 
-    public IState<Calculator> Calculator => State.Value(this, () => new Calculator());
+    public IState<Calculator> Calculator { get; }
+
     public async ValueTask Input(KeyInput key, CancellationToken ct)
             => await Calculator.Update(c => c?.Input(key), ct);
 
@@ -22,6 +18,9 @@ public partial record MainModel
             => await Calculator.Update(c => c?.Input(key), ct);
     public MainModel()
     {
+        Calculator = State.Value(this, () => new Calculator());
+        IsDark = State.Value(this, () => _theme.IsDark);
+
         IsDark.ForEachAsync((dark, ct) => _theme.SetThemeAsync(dark, ct));
     }
 
