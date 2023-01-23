e77d3b949258b6509e993b79269b78e2b5423060
esteves <aesteves900@gmail.com>
2023-01-23 13:09:23 -0300

    feat: replace the MapControl to Mapsui control

--- UI/MapControlSample/MapControlSample/MapControlSample.Shared/MainPage.xaml.cs
+++ UI/MapControlSample/MapControlSample/MapControlSample.Shared/MainPage.xaml.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Devices.Geolocation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
+﻿using Mapsui;
+using Mapsui.Extensions;
+using Mapsui.Projections;
+using Mapsui.Tiling;
+using Mapsui.UI;
+using Mapsui.Widgets.Zoom;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,7 +21,83 @@ namespace MapControlSample
         {
             this.InitializeComponent();
 
-            MapControl.Center = new Geopoint(new BasicGeoposition { Latitude = 45.491912, Longitude = -73.551542 });
+            zoomSlider.ValueChanged += ZoomSlider_ValueChanged;
+
+            MapControl.PointerWheelChanged += MapControl_PointerWheelChanged;
+            MapControl.Map.Layers.Add(OpenStreetMap.CreateTileLayer());
+            MapControl.Map.Widgets.Add(new ZoomInOutWidget { MarginX = 10, MarginY = 20 });
+            MapControl.PointerMoved += MapControl_PointerMoved;
+            var centerOfLondonOntario = new MPoint(-81.2497, 42.9837);
+
+            var sphericalMercatorCoordinate = SphericalMercator.FromLonLat(centerOfLondonOntario.X, centerOfLondonOntario.Y).ToMPoint();
+
+            MapControl.Map.Home = n => n.NavigateTo(sphericalMercatorCoordinate, MapControl.Map.Resolutions[13]);
         }
+
+        private void MapControl_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            _currentPoint = e.GetCurrentPoint(this);
+        }
+
+        private void ZoomSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            var mousePosition = new MPoint(_currentPoint.Position.X, _currentPoint.Position.Y);
+
+            if (MapControl.Map == null)
+            {
+                return;
+            }
+            if (e.NewValue > e.OldValue)
+            {
+                MapControl.Navigator?.ZoomIn(mousePosition, 100, MouseWheelAnimation.Easing);
+            }
+            else
+            {
+                MapControl.Navigator?.ZoomOut(mousePosition, 100, MouseWheelAnimation.Easing);
+            }
+        }
+
+        private void MapControl_PointerWheelChanged(object sender, PointerRoutedEventArgs e)
+        {
+            if (MapControl.Map?.ZoomLock ?? true)
+            {
+                return;
+            }
+
+            if (!Viewport.HasSize())
+            {
+                return;
+            }
+
+            if (_currentPoint is null)
+            {
+                _currentPoint = e.GetCurrentPoint(this);
+            }
+
+            var mousePosition = new MPoint(_currentPoint.Position.X, _currentPoint.Position.Y);
+
+            var resolution = MouseWheelAnimation.GetResolution(_currentPoint.Properties.MouseWheelDelta, _viewport, MapControl.Map);
+
+            if (MapControl.Map == null)
+            {
+                return;
+            }
+
+            resolution = MapControl.Map.Limiter.LimitResolution(resolution, Viewport.Width, Viewport.Height, MapControl.Map.Resolutions, MapControl.Map.Extent);
+            MapControl.Navigator?.ZoomTo(resolution, mousePosition, 100, MouseWheelAnimation.Easing);
+
+            e.Handled = true;
+        }
+
+        public MouseWheelAnimation MouseWheelAnimation { get; } = new MouseWheelAnimation { Duration = 0 };
+
+        private Microsoft.UI.Input.PointerPoint _currentPoint;
+
+        /// <summary>
+        /// Viewport holding information about visible part of the map. Viewport can never be null.
+        /// </summary>
+        public IReadOnlyViewport Viewport => _viewport;
+
+        private readonly LimitedViewport _viewport = new LimitedViewport();
     }
 }
