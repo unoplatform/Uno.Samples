f10d260f59e7db65519ea0b1086169a2b0860308
esteves <aesteves900@gmail.com>
2023-01-24 13:35:13 -0300

    Update UI/MapControlSample/MapControlSample/MapControlSample.Shared/App.xaml.cs
    Co-authored-by: Agnès ZITTE <16295702+agneszitte-nventive@users.noreply.github.com>

--- UI/MapControlSample/MapControlSample/MapControlSample.Shared/App.xaml.cs
+++ UI/MapControlSample/MapControlSample/MapControlSample.Shared/App.xaml.cs
@@ -119,8 +119,8 @@ namespace MapControlSample
 #if DEBUG
             // Logging is disabled by default for release builds, as it incurs a significant
             // initialization cost from Microsoft.Extensions.Logging setup. If startup performance
-            // is a concern for your application, keep this disabled. If you're running on web or 
-            // desktop targets, you can use url or command line parameters to enable it.
+            // is a concern for your application, keep this disabled. If you're running on the web or 
+            // desktop targets, you can use URL or command line parameters to enable it.
             //
             // For more performance documentation: https://platform.uno/docs/articles/Uno-UI-Performance.html
 
