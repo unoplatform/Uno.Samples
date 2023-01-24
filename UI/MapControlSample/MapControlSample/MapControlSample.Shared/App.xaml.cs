1dd70b2e8b943b1ba620dfd5d2d3e0a08b2a0698
esteves <aesteves900@gmail.com>
2023-01-24 13:35:23 -0300

    Update UI/MapControlSample/MapControlSample/MapControlSample.Shared/App.xaml.cs
    Co-authored-by: Agnès ZITTE <16295702+agneszitte-nventive@users.noreply.github.com>

--- UI/MapControlSample/MapControlSample/MapControlSample.Shared/App.xaml.cs
+++ UI/MapControlSample/MapControlSample/MapControlSample.Shared/App.xaml.cs
@@ -107,7 +107,7 @@ namespace MapControlSample
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
+            //TODO: Save the application state and stop any background activity
             deferral.Complete();
         }
 
