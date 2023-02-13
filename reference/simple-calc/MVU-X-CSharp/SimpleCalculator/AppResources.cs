3b9544707791fbd153c7648b82b0a4d07ee67126
Dan Siegel <me@dansiegel.net>
2023-02-13 11:27:21 -0600

    chore: adding generated files from reference directory

---
+++ reference/simple-calc/MVU-X-CSharp/SimpleCalculator/AppResources.cs
@@ -0,0 +1,27 @@
+﻿using Uno.Toolkit.UI;
+using Uno.Material;
+
+namespace SimpleCalculator;
+
+public sealed class AppResources : ResourceDictionary
+{
+	public AppResources()
+	{
+		// Load WinUI Resources
+		this.Build(r => r.Merged(
+			new XamlControlsResources()));
+
+		// Load Material Resources
+		this.Build(r => r.Merged(
+			new MaterialTheme()
+				.ColorOverrideDictionary(new Styles.ColorPaletteOverride())));
+
+		// Load Uno.UI.Toolkit Resources
+		this.Build(r => r.Merged(
+			new ToolkitResources()));
+
+		// Load Uno.UI.Toolkit Resources
+		this.Build(r => r.Merged(
+			new ToolkitResources()));
+    }
+}
---
+++ reference/simple-calc/MVU-X-Xaml/SimpleCalculator/Presentation/MainModel.cs
@@ -0,0 +1,29 @@
+﻿using SimpleCalculator.Business;
+using Uno.Extensions.Reactive;
+using Uno.Extensions.Toolkit;
+
+namespace SimpleCalculator.Presentation;
+
+public partial record MainModel
+{
+    public IState<bool> IsDark { get; }
+
+    public IState<Calculator> Calculator { get; }
+
+    public async ValueTask InputCommand(string key, CancellationToken ct)
+            => await Calculator.Update(c => c?.Input(key), ct);
+
+    public MainModel(IThemeService themeService)
+    {
+        Calculator = State.Value(this, () => new Calculator());
+        IsDark = State.Value(this, () => themeService.IsDark);
+
+        themeService.ThemeChanged += async (_, _) =>
+            await IsDark.Update(_ => themeService.IsDark, CancellationToken.None);
+
+        IsDark.ForEachAsync(async (dark, ct) => await themeService.SetThemeAsync(dark ? AppTheme.Dark : AppTheme.Light));
+
+        
+    }
+}
+
