573d9e395bb21e5f21cf332499c257948e4b1c00
Peter Freeman Foot <peter@inthehand.com>
2023-02-22 19:21:30 +0000

    Initial code for Bluetooth Explorer

---
+++ UI/BluetoothExplorer/UnoBluetoothExplorer.Mobile/iOS/Main.iOS.cs
@@ -0,0 +1,21 @@
+﻿using Foundation;
+using Network;
+using System.Diagnostics;
+using UIKit;
+
+namespace UnoBluetoothExplorer
+{
+    public class EntryPoint
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            var hasInfoKey = CoreFoundation.CFBundle.GetMain().InfoDictionary.ContainsKey(new NSString("NSBluetoothAlwaysUsageDescription"));
+            Debug.WriteLine(hasInfoKey);
+            
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, typeof(AppHead));
+        }
+    }
+}
