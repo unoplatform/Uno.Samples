eb1c2b4189676cfcf6c928ea108b351e10b500ab
Steve Bilogan <steve.bilogan@gmail.com>
2023-03-03 22:49:27 -0500

    chore: Migrate Material sample to new solution template

---
+++ UI/UnoMaterialSample/UnoMaterialSample/UnoMaterialSample/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace UnoMaterialSample
+{
+	public sealed partial class MainPage : Page
+	{
+		public List<int> Items => Enumerable.Range(0, 10).ToList();
+
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
