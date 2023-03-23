ef0f3af164165d5888b3bb25035f41fe649d38cf
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2023-03-23 02:43:15 +0000

    feat: Add MVUX Simple feed Weather sample

---
+++ UI/MvuxHowTos/WeatherApp/WeatherApp/WeatherModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp;
+
+using Uno.Extensions.Reactive;
+
+public partial record WeatherModel(WeatherService WeatherService)
+{
+	public IFeed<WeatherInfo> CurrentWeather => Feed.Async(WeatherService.GetCurrentWeatherAsync);
+}
---
+++ UI/MvuxHowTos/WeatherApp/WeatherApp/WeatherService.cs
@@ -0,0 +1,17 @@
+﻿namespace WeatherApp;
+
+public partial record WeatherInfo(int Temperature);
+
+public class WeatherService
+{
+	public async ValueTask<WeatherInfo> GetCurrentWeatherAsync(CancellationToken ct)
+	{
+		// fake delay to simulate requesting data from a remote server
+		await Task.Delay(TimeSpan.FromSeconds(2), ct);
+
+		// assign a random number ranged -40 to 40.
+		var temperature = new Random().Next(-40, 40);
+
+		return new WeatherInfo(temperature);
+	}
+}
