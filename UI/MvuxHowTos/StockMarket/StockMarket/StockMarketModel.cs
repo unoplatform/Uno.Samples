2d87ed8cea183288834e38098649e9a54c261caf
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2023-03-23 05:26:57 +0000

    feat: Add MVUX push list-feed StockMarket sample

---
+++ UI/MvuxHowTos/StockMarket/StockMarket/StockMarketModel.cs
@@ -0,0 +1,8 @@
+﻿using Uno.Extensions.Reactive;
+
+namespace StockMarket;
+
+public partial record StockMarketModel(StockMarketService StockMarketService)
+{
+    public IListFeed<Stock> Stocks => ListFeed.AsyncEnumerable(StockMarketService.GetCurrentMarket);
+}
