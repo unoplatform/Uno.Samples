07f44149493f7c70806c3ebc4ca6ec6454fc552b
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2023-03-24 17:28:04 +0000

    fix: Exchange concrete with interface

--- UI/MvuxHowTos/TheFancyWeddingHall/TheFancyWeddingHall/HallCrowdednessModel.cs
+++ UI/MvuxHowTos/TheFancyWeddingHall/TheFancyWeddingHall/HallCrowdednessModel.cs
@@ -2,7 +2,7 @@
 
 namespace TheFancyWeddingHall;
 
-public partial record HallCrowdednessModel(HallCrowdednessService HallCrowdednessService)
+public partial record HallCrowdednessModel(IHallCrowdednessService HallCrowdednessService)
 {
     public IState<HallCrowdedness> HallCrowdedness => State.Async(this, HallCrowdednessService.GetHallCrowdedness);
 
