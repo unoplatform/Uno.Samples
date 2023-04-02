5e90453671f0b570c97ea18b68c56c921e5c16c1
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2023-04-02 08:54:22 +0100

    refactor: Added additional command to SliderApp

--- UI/MvuxHowTos/SliderApp/SliderApp/SliderModel.cs
+++ UI/MvuxHowTos/SliderApp/SliderApp/SliderModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Immutable;
 using Uno.Extensions.Reactive;
+using Windows.Security.Cryptography.Core;
 
 namespace SliderApp;
 
@@ -9,6 +10,16 @@ public partial record SliderModel
 
     public async ValueTask ResetSlider(CancellationToken ct = default)
     {
-        await SliderValue.Update(updater: currentValue => 0, ct);
+        await SliderValue.Set(value: 0, ct);
+    }
+
+    public async ValueTask IncrementSlider(CancellationToken ct = default)
+    {
+        static double incrementValue(double currentValue) =>
+            currentValue <= 99
+            ? currentValue + 1
+            : 1;
+
+        await SliderValue.Update(updater: incrementValue, ct);
     }
 }
