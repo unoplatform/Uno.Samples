ef444ef93271f2ef36887611d1ea1a1575592692
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2023-04-02 07:11:57 +0100

    fix: Added Command to MVUX SliderApp

--- UI/MvuxHowTos/SliderApp/SliderApp/SliderModel.cs
+++ UI/MvuxHowTos/SliderApp/SliderApp/SliderModel.cs
@@ -1,8 +1,14 @@
-﻿using Uno.Extensions.Reactive;
+﻿using System.Collections.Immutable;
+using Uno.Extensions.Reactive;
 
 namespace SliderApp;
 
 public partial record SliderModel
 {
     public IState<double> SliderValue => State.Value(this, () => Random.Shared.NextDouble() * 100);
+
+    public async ValueTask ResetSlider(CancellationToken ct = default)
+    {
+        await SliderValue.Update(updater: currentValue => 0, ct);
+    }
 }
