a7768d2f4e9544f2875b614c57175b09bb66d5c6
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2023-04-20 12:53:40 +0100

    fix: Separated selection and pagination projects - touch up selection project

---
+++ UI/MvuxHowTos/PaginationPeopleApp/PaginationPeopleApp/PeopleModel.cs
@@ -0,0 +1,15 @@
+﻿using Uno.Extensions.Reactive;
+
+namespace PaginationPeopleApp;
+
+public partial record PeopleModel(IPeopleService PeopleService)
+{
+    public IListFeed<Person> People =>
+        ListFeed.AsyncPaginated(async (pageRequest, ct) =>
+            await PeopleService.GetPeopleAsync(pageSize: 5, pageIndex: pageRequest.Index, ct));    
+
+    public ValueTask MovePage(int direction, CancellationToken ct)
+    {
+        return ValueTask.CompletedTask;
+    }
+}
--- UI/MvuxHowTos/SelectionPeopleApp/SelectionPeopleApp/PeopleModel.cs
+++ UI/MvuxHowTos/SelectionPeopleApp/SelectionPeopleApp/PeopleModel.cs
@@ -4,24 +4,13 @@ namespace SelectionPeopleApp;
 
 public partial record PeopleModel(IPeopleService PeopleService)
 {
-    public IListFeed<Person> People => ListFeed.Async(PeopleService.GetPeopleAsync);
-
-    /*
     public IListFeed<Person> People =>
         ListFeed
         .Async(PeopleService.GetPeopleAsync)
-        .Selection(SelectedPeople);
-    */
-
-    //public IState<string> SelectedFirstName => State<string>.Empty(this);
+        .Selection(SelectedPerson);
+        //.Selection(SelectedPeople);
 
     public IState<Person> SelectedPerson => State<Person>.Empty(this);
 
     public IState<IImmutableList<Person>> SelectedPeople => State<IImmutableList<Person>>.Empty(this);
-
-    public IState<int> DesiredSize => State<int>.Value(this, () => 5);
-
-    public IListFeed<Person> PaginatedPeople =
-        ListFeed.AsyncPaginated(async (pageRequest, ct) =>
-            await PeopleService.GetPeopleAsync(pageRequest.DesiredSize ?? 0, pageRequest.Index, ct));
 }
