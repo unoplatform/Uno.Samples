6e98138d8b9af9613751da349ae01a51f874c26b
Matt <90909523+mtmattei@users.noreply.github.com>
2023-04-29 06:40:56 -0400

    Neumorphism

---
+++ UI/Neumorphism/UnoApp1/UnoApp1.Base/IsExternalInit.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+
+#if NETSTANDARD
+namespace System.Runtime.CompilerServices;
+
+internal static class IsExternalInit
+{
+}
+#endif
---
+++ UI/Neumorphism/UnoApp1/UnoApp1.Mobile/Android/MainActivity.Android.cs
@@ -0,0 +1,17 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+
+namespace UnoApp1.Droid
+{
+    [Activity(
+        MainLauncher = true,
+        ConfigurationChanges = global::Uno.UI.ActivityHelper.AllConfigChanges,
+        WindowSoftInputMode = SoftInput.AdjustNothing | SoftInput.StateHidden
+    )]
+    public class MainActivity : Microsoft.UI.Xaml.ApplicationActivity
+    {
+    }
+}
