61ea07b96e0b01f29e8c9a1a3a59e4196b950cd1
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2023-05-07 20:18:12 +0100

    Apply more changes to Selection

--- UI/MvuxHowTos/SelectionPeopleApp/SelectionPeopleApp/PeopleModel.cs
+++ UI/MvuxHowTos/SelectionPeopleApp/SelectionPeopleApp/PeopleModel.cs
@@ -1,16 +1,42 @@
 ﻿using Uno.Extensions.Reactive;
+using Windows.UI.Popups;
 
 namespace SelectionPeopleApp;
 
-public partial record PeopleModel(IPeopleService PeopleService)
+public partial record PeopleModel
 {
+    private IPeopleService _peopleService;
+
+    public PeopleModel(IPeopleService peopleService)
+    {
+        _peopleService = peopleService;
+
+        SelectedPerson.ForEachAsync(action: SelectionChanged);
+    }
+
     public IListFeed<Person> People =>
         ListFeed
-        .Async(PeopleService.GetPeopleAsync)
+        .Async(_peopleService.GetPeopleAsync)
         .Selection(SelectedPerson);
+    // multi selection
     //.Selection(SelectedPeople);
 
     public IState<Person> SelectedPerson => State<Person>.Empty(this);
 
     public IState<IImmutableList<Person>> SelectedPeople => State<IImmutableList<Person>>.Empty(this);
+
+    public IFeed<string> GreetingSelect =>
+        SelectedPerson
+        .Where(person => person != null)
+        .Select(person => $"Hello {person.FirstName} {person.LastName}!");
+
+
+    public IState<string> GreetingForEach => State.Value(this, () => string.Empty);
+    public async ValueTask SelectionChanged(Person? selectedPerson, CancellationToken ct)
+    {
+        if (selectedPerson == null)
+            return;
+
+        await GreetingForEach.Set($"Hello {selectedPerson.FirstName} {selectedPerson.LastName}!", ct);
+    }
 }
