47ae5b0717de25ce1daf98bfefba8acbda5bac91
Shimmy <2716316+weitzhandler@users.noreply.github.com>
2023-06-19 02:37:59 +0100

    feat(mvux): Messaging sample app initial creation

---
+++ UI/MvuxHowTos/MessagingPeopleApp/MessagingPeopleApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace MessagingPeopleApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
