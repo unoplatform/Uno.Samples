ccfa757865524698badbcf31c3e261d36f3eaad6
Nick Randolph <nick@builttoroam.com>
2023-07-14 02:09:48 +1000

    fix: Missing props file and remove deprecated args parameter

--- UI/MigratingAnimations/MigratingAnimations.Skia.Gtk/Program.cs
+++ UI/MigratingAnimations/MigratingAnimations.Skia.Gtk/Program.cs
@@ -14,7 +14,7 @@ namespace MigratingAnimations.Skia.Gtk
                 expArgs.ExitApplication = true;
             };
 
-            var host = new GtkHost(() => new AppHead(), args);
+            var host = new GtkHost(() => new AppHead());
 
             host.Run();
         }
