94495eb486aa2b4aa8d7fa8ac926031874280c1b
Youssef Victor <youssefvictor00@gmail.com>
2023-07-30 07:29:37 +0300

    chore: Fix canaries

--- UI/SkiaSharpTest/SkiaSharpTest/SkiaSharpTest.Skia.WPF/MainWindow.xaml.cs
+++ UI/SkiaSharpTest/SkiaSharpTest/SkiaSharpTest.Skia.WPF/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace SkiaSharpTest.WPF
         {
             InitializeComponent();
 
-            root.Content = new global::Uno.UI.Skia.Platform.WpfHost(Dispatcher, () => new SkiaSharpTest.App());
+            root.Content = new global::Uno.UI.Runtime.Skia.Wpf.WpfHost(Dispatcher, () => new SkiaSharpTest.App());
         }
     }
 }
