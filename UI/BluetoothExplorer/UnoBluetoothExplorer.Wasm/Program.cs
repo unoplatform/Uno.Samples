94d56befb57d36c2306d9bc0d5ec9a8db0c91092
Matthew Rajala <matthew.rajala@nventive.com>
2023-08-07 10:56:13 -0400

    chore: add skia and wasm targets bluetooth

---
+++ UI/BluetoothExplorer/UnoBluetoothExplorer.Wasm/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+
+namespace UnoBluetoothExplorer.Wasm
+{
+    public class Program
+    {
+        private static App _app;
+
+        static int Main(string[] args)
+        {
+            Microsoft.UI.Xaml.Application.Start(_ => _app = new App());
+
+            return 0;
+        }
+    }
+}
