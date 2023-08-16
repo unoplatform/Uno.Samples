47897cf9240ca67319c4a4d11bd8bc1803c04899
Marc-Antoine Soucy <ma.soucy@nventive.com>
2023-08-16 10:00:43 -0400

    fix: liteDB formatting.

--- UI/LiteDB/LiteDB/src/LiteDBSample.Shared/MainPage.xaml.cs
+++ UI/LiteDB/LiteDB/src/LiteDBSample.Shared/MainPage.xaml.cs
@@ -8,64 +8,64 @@ using LiteDB;
 
 namespace LiteDBSample
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        private readonly ObservableCollection<TodoItem> _todoItems;
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		private readonly ObservableCollection<TodoItem> _todoItems;
 
-        public MainPage()
-        {
-            InitializeComponent();
-            _todoItems = new ObservableCollection<TodoItem>();
-        }
+		public MainPage()
+		{
+			InitializeComponent();
+			_todoItems = new ObservableCollection<TodoItem>();
+		}
 
-        private LiteDatabase _liteDatabase;
+		private LiteDatabase _liteDatabase;
 
-        private LiteDatabase LiteDatabase
-        {
-            get
-            {
-                // If the database is created in the constructor, it stops working on WASM.
-                if (_liteDatabase == null)
-                {
-                    _liteDatabase = new LiteDatabase(DbPath);
-                }
-                return _liteDatabase;
-            }
-        }
+		private LiteDatabase LiteDatabase
+		{
+			get
+			{
+				// If the database is created in the constructor, it stops working on WASM.
+				if (_liteDatabase == null)
+				{
+					_liteDatabase = new LiteDatabase(DbPath);
+				}
+				return _liteDatabase;
+			}
+		}
 
-        private static string DbPath => System.IO.Path.Combine(ApplicationData.Current.LocalFolder.Path, "save.db");
+		private static string DbPath => System.IO.Path.Combine(ApplicationData.Current.LocalFolder.Path, "save.db");
 
-        private void LoadFromDatabase(object sender, RoutedEventArgs args)
-        {
-            var liteCollection = LiteDatabase.GetCollection<TodoItem>();
-            var todoItems = liteCollection
-                .FindAll()
-                .ToList();
-            _todoItems.Clear();
-            todoItems.ForEach(t => _todoItems.Add(t));
-        }
+		private void LoadFromDatabase(object sender, RoutedEventArgs args)
+		{
+			var liteCollection = LiteDatabase.GetCollection<TodoItem>();
+			var todoItems = liteCollection
+				.FindAll()
+				.ToList();
+			_todoItems.Clear();
+			todoItems.ForEach(t => _todoItems.Add(t));
+		}
 
-        private void DeleteFromDatabase(object sender, RoutedEventArgs args)
-        {
-            LiteDatabase.GetCollection<TodoItem>().DeleteAll();
-            _todoItems.Clear();
-        }
+		private void DeleteFromDatabase(object sender, RoutedEventArgs args)
+		{
+			LiteDatabase.GetCollection<TodoItem>().DeleteAll();
+			_todoItems.Clear();
+		}
 
-        private void AddTodoItem(object sender, RoutedEventArgs args)
-        {
-            var todoItem = new TodoItem
-            {
-                Text = todoText.Text
-            };
+		private void AddTodoItem(object sender, RoutedEventArgs args)
+		{
+			var todoItem = new TodoItem
+			{
+				Text = todoText.Text
+			};
 
-            todoText.Text = string.Empty;
-            var liteCollection = LiteDatabase.GetCollection<TodoItem>();
-            liteCollection.Insert(todoItem);
-            LiteDatabase.Commit();
-            _todoItems.Add(todoItem);
-        }
-    }
+			todoText.Text = string.Empty;
+			var liteCollection = LiteDatabase.GetCollection<TodoItem>();
+			liteCollection.Insert(todoItem);
+			LiteDatabase.Commit();
+			_todoItems.Add(todoItem);
+		}
+	}
 }
