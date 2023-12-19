f78d2fcbadbe830dac08cfa87adeee55e518c74b
Nick Randolph <nick@builttoroam.com>
2023-12-19 23:13:54 +1100

    chore: Fix build errors and warning

--- UI/MapControlSample/MapControlSample/MapControlSample.Skia.Linux.FrameBuffer/Program.cs
+++ UI/MapControlSample/MapControlSample/MapControlSample.Skia.Linux.FrameBuffer/Program.cs
@@ -1,41 +1,40 @@
 using Microsoft.UI.Xaml;
 using System;
-using Uno.UI.Runtime.Skia;
+using Uno.UI.Runtime.Skia.Linux.FrameBuffer;
 using Windows.UI.Core;
 
-namespace MapControlSample
+namespace MapControlSample;
+
+class Program
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			try
-			{
-				Console.CursorVisible = false;
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.CursorVisible = false;
 
-				var host = new FrameBufferHost(() =>
-				{
-					// Framebuffer applications don't have a WindowManager to rely
-					// on. To close the application, we can hook onto CoreWindow events
-					// which dispatch keyboard input, and close the application as a result.
-					// This block can be moved to App.xaml.cs if it does not interfere with other
-					// platforms that may use the same keys.
-					CoreWindow.GetForCurrentThread().KeyDown += (s, e) =>
-					{
-						if (e.VirtualKey == Windows.System.VirtualKey.F12)
-						{
-							Application.Current.Exit();
-						}
-					};
+            var host = new FrameBufferHost(() =>
+            {
+                // Framebuffer applications don't have a WindowManager to rely
+                // on. To close the application, we can hook onto CoreWindow events
+                // which dispatch keyboard input, and close the application as a result.
+                // This block can be moved to App.xaml.cs if it does not interfere with other
+                // platforms that may use the same keys.
+                CoreWindow.GetForCurrentThread().KeyDown += (s, e) =>
+                {
+                    if (e.VirtualKey == Windows.System.VirtualKey.F12)
+                    {
+                        Application.Current.Exit();
+                    }
+                };
 
-					return new App();
-				});
-				host.Run();
-			}
-			finally
-			{
-				Console.CursorVisible = true;
-			}
-		}
-	}
+                return new App();
+            });
+            host.Run();
+        }
+        finally
+        {
+            Console.CursorVisible = true;
+        }
+    }
 }
--- UI/MapControlSample/MapControlSample/MapControlSample.Skia.WPF/MainWindow.xaml.cs
+++ UI/MapControlSample/MapControlSample/MapControlSample.Skia.WPF/MainWindow.xaml.cs
@@ -1,31 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Uno.UI.Skia.Platform;
+using Uno.UI.Runtime.Skia.Wpf;
 
-namespace MapControlSample.WPF
+namespace MapControlSample.WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-	
-			root.Content = new WpfHost(Dispatcher, () => new MapControlSample.App());
-		}
-	}
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        root.Content = new WpfHost(Dispatcher, () => new MapControlSample.App());
+    }
 }
