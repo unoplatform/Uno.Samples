aae82a729f7c6a927f8508d15c17d75d3cbb07db
eriklimakc <erik.lima@knowcode.app>
2024-01-18 15:40:01 +0000

    chore: Remove server project

--- UI/ChatGPT/ChatGPT/ChatGPT.Server/GlobalUsings.cs
+++
@@ -1,3 +0,0 @@
-global using ChatGPT.DataContracts;
-global using ChatGPT.Server.Apis;
-global using Microsoft.AspNetCore.Mvc;
--- UI/ChatGPT/ChatGPT/ChatGPT/Presentation/MainPage.cs
+++ UI/ChatGPT/ChatGPT/ChatGPT/Presentation/MainPage.cs
@@ -4,103 +4,104 @@ namespace ChatGPT.Presentation;
 
 public sealed partial class MainPage : Page
 {
-    public MainPage()
-    {
-        this.Background(ThemeResource.Get<Brush>("ApplicationPageBackgroundThemeBrush"))
-            .DataContext<BindableMainModel>((page, vm) => page
-                .Content(
-                    new Grid().BorderBrush(Colors.Gray).BorderThickness(1).CornerRadius(10)
-                        .MaxWidth(500)
-                        .Padding(10).Margin(0, 10)
-                        .RowDefinitions<Grid>("*,Auto")
-                        .Children(
-                            new ListView()
-                                .VerticalAlignment(VerticalAlignment.Bottom)
-                                .SelectionMode(ListViewSelectionMode.None)
-                                .ItemsSource(() => vm.Messages)
-                                .AutoScrollListViewBehavior(autoScroll: true)
-                                .ItemTemplateSelector<Message>((message, selector) => selector
-                                    .Default(() => new TextBlock().Text(() => message.Content))
-                                    .Case(
-                                        m => m.Source == Source.User,
-                                        () => new StackPanel()
-                                            .Margin(8)
-                                            .HorizontalAlignment(HorizontalAlignment.Right)
-                                            .Children(
-                                                new Border()
-                                                    .Background(Colors.DarkSlateGray)
-                                                    .CornerRadius(10)
-                                                    .MinWidth(70)
-                                                    .MaxWidth(350)
-                                                    .Padding(10)
-                                                    .Child(
-                                                        new TextBlock()
-                                                            .Foreground(Colors.Yellow)
-                                                            .HorizontalAlignment(HorizontalAlignment.Center)
-                                                            .TextWrapping(TextWrapping.Wrap)
-                                                            .Text(() => message.Content))
-                                            )
-                                    )
-                                    .Case(
-                                        m => m.Source == Source.AI && m.Status != Status.Error,
-                                        () => new StackPanel()
-                                            .Margin(8)
-                                            .HorizontalAlignment(HorizontalAlignment.Left)
-                                            .Children(
-                                                new Border()
-                                                    .Background(Colors.DarkGray)
-                                                    .CornerRadius(10)
-                                                    .MinWidth(70)
-                                                    .MaxWidth(350)
-                                                    .Padding(10)
-                                                    .Child(
-                                                        new TextBlock()
-                                                            .Foreground(Colors.Black)
-                                                            .HorizontalAlignment(HorizontalAlignment.Center)
-                                                            .TextWrapping(TextWrapping.Wrap)
-                                                            .Text(() => message.Content)
-                                                    )
-                                            )
-                                    )
-                                    .Case(
-                                        m => m.Source == Source.AI && m.Status == Status.Error,
-                                        () => new StackPanel()
-                                            .Margin(8)
-                                            .HorizontalAlignment(HorizontalAlignment.Left)
-                                            .Children(
-                                                new Border()
-                                                    .Background(Colors.DarkGray)
-                                                    .CornerRadius(10)
-                                                    .MinWidth(70)
-                                                    .MaxWidth(350)
-                                                    .Padding(10)
-                                                    .Child(
-                                                        new TextBlock()
-                                                            .Foreground(Colors.Red)
-                                                            .HorizontalAlignment(HorizontalAlignment.Center)
-                                                            .TextWrapping(TextWrapping.Wrap)
-                                                            .Text(() => message.Content)
-                                                    )
-                                            )
-                                    )
-                                ),
-                            new StackPanel()
-                                .Grid(row: 1)
-                                .VerticalAlignment(VerticalAlignment.Bottom)
-                                .HorizontalAlignment(HorizontalAlignment.Center)
-                                .Orientation(Orientation.Horizontal)
-                                .Spacing(10)
-                                .Children(
-                                    new TextBox()
-                                        .Width(300)
-                                        .PlaceholderText("Message ChatGPT")
-                                        .CommandExtensions(x => x.Command(() => vm.SendMessage))
-                                        .Text(x => x.Bind(() => vm.Prompt).TwoWay().UpdateSourceTrigger(UpdateSourceTrigger.PropertyChanged)),
-                                    new Button()
-                                        .Content("Send")
-                            )
-                    )
-                )
-            );
-    }
+	public MainPage()
+	{
+		this.Background(ThemeResource.Get<Brush>("ApplicationPageBackgroundThemeBrush"))
+			.DataContext<BindableMainModel>((page, vm) => page
+				.Content(
+					new Grid().BorderBrush(Colors.Gray).BorderThickness(1).CornerRadius(10)
+						.MaxWidth(500)
+						.Padding(10).Margin(0, 10)
+						.RowDefinitions<Grid>("*,Auto")
+						.Children(
+							new ListView()
+								.VerticalAlignment(VerticalAlignment.Bottom)
+								.SelectionMode(ListViewSelectionMode.None)
+								.ItemsSource(() => vm.Messages)
+								.AutoScrollListViewBehavior(autoScroll: true)
+								.ItemTemplateSelector<Message>((message, selector) => selector
+									.Default(() => new TextBlock().Text(() => message.Content))
+									.Case(
+										m => m.Source == Source.User,
+										() => new StackPanel()
+											.Margin(8)
+											.HorizontalAlignment(HorizontalAlignment.Right)
+											.Children(
+												new Border()
+													.Background(Colors.DarkSlateGray)
+													.CornerRadius(10)
+													.MinWidth(70)
+													.MaxWidth(350)
+													.Padding(10)
+													.Child(
+														new TextBlock()
+															.Foreground(Colors.Yellow)
+															.HorizontalAlignment(HorizontalAlignment.Center)
+															.TextWrapping(TextWrapping.Wrap)
+															.Text(() => message.Content))
+											)
+									)
+									.Case(
+										m => m.Source == Source.AI && m.Status != Status.Error,
+										() => new StackPanel()
+											.Margin(8)
+											.HorizontalAlignment(HorizontalAlignment.Left)
+											.Children(
+												new Border()
+													.Background(Colors.DarkGray)
+													.CornerRadius(10)
+													.MinWidth(70)
+													.MaxWidth(350)
+													.Padding(10)
+													.Child(
+														new TextBlock()
+															.Foreground(Colors.Black)
+															.HorizontalAlignment(HorizontalAlignment.Center)
+															.TextWrapping(TextWrapping.Wrap)
+															.Text(() => message.Content)
+													)
+											)
+									)
+									.Case(
+										m => m.Source == Source.AI && m.Status == Status.Error,
+										() => new StackPanel()
+											.Margin(8)
+											.HorizontalAlignment(HorizontalAlignment.Left)
+											.Children(
+												new Border()
+													.Background(Colors.DarkGray)
+													.CornerRadius(10)
+													.MinWidth(70)
+													.MaxWidth(350)
+													.Padding(10)
+													.Child(
+														new TextBlock()
+															.Foreground(Colors.Red)
+															.HorizontalAlignment(HorizontalAlignment.Center)
+															.TextWrapping(TextWrapping.Wrap)
+															.Text(() => message.Content)
+													)
+											)
+									)
+								),
+							new StackPanel()
+								.Grid(row: 1)
+								.VerticalAlignment(VerticalAlignment.Bottom)
+								.HorizontalAlignment(HorizontalAlignment.Center)
+								.Orientation(Orientation.Horizontal)
+								.Spacing(10)
+								.Children(
+									new TextBox()
+										.Width(300)
+										.PlaceholderText("Message ChatGPT")
+										.CommandExtensions(x => x.Command(() => vm.SendMessage))
+										.Text(x => x.Bind(() => vm.Prompt).TwoWay().UpdateSourceTrigger(UpdateSourceTrigger.PropertyChanged)),
+									new Button()
+										.Command(() => vm.SendMessage)
+										.Content("Send")
+							)
+					)
+				)
+			);
+	}
 }
