8f06152a6e122c57526d5b45224fd260b0b009db
Nick Randolph <nick@builttoroam.com>
2024-03-29 00:41:07 +1100

    chore: Adding mobile targets to class lib

--- reference/ToDo/src/ToDo/Presentation/TaskViewModel.cs
+++ reference/ToDo/src/ToDo/Presentation/TaskViewModel.cs
@@ -1,86 +1,88 @@
-﻿namespace ToDo.Presentation;
+﻿using Dialog = ToDo.Presentation.Dialogs.Dialog;
+
+namespace ToDo.Presentation;
 
 public partial class TaskViewModel
 {
-	private readonly INavigator _navigator;
-	private readonly ITaskService _svc;
+    private readonly INavigator _navigator;
+    private readonly ITaskService _svc;
 
-	public TaskViewModel(
-		INavigator navigator,
-		ITaskService svc,
-		ToDoTask entity)
-	{
-		_navigator = navigator;
-		_svc = svc;
+    public TaskViewModel(
+        INavigator navigator,
+        ITaskService svc,
+        ToDoTask entity)
+    {
+        _navigator = navigator;
+        _svc = svc;
 
-		Entity = State.Value(this, () => entity);
-		Entity.Execute(async (task, ct) =>
-		{
-			if (task is not null)
-			{
-				await _svc.UpdateAsync(task, ct);
-			}
-		});
-	}
+        Entity = State.Value(this, () => entity);
+        Entity.Execute(async (task, ct) =>
+        {
+            if (task is not null)
+            {
+                await _svc.UpdateAsync(task, ct);
+            }
+        });
+    }
 
-	public IState<ToDoTask> Entity { get; }
+    public IState<ToDoTask> Entity { get; }
 
-	public async ValueTask ToggleIsCompleted(CancellationToken ct)
-	{
-		if (await Entity is { } task)
-		{
-			await _svc.UpdateAsync(task.ToggleIsCompleted(), ct);
-		}
-	}
+    public async ValueTask ToggleIsCompleted(CancellationToken ct)
+    {
+        if (await Entity is { } task)
+        {
+            await _svc.UpdateAsync(task.ToggleIsCompleted(), ct);
+        }
+    }
 
-	public async ValueTask ToggleIsImportant(CancellationToken ct)
-	{
-		if (await Entity is { } task)
-		{
-			await _svc.UpdateAsync(task.ToggleIsImportant(), ct);
-		}
-	}
+    public async ValueTask ToggleIsImportant(CancellationToken ct)
+    {
+        if (await Entity is { } task)
+        {
+            await _svc.UpdateAsync(task.ToggleIsImportant(), ct);
+        }
+    }
 
-	public async ValueTask Delete(CancellationToken ct)
-	{
-		var task = await Entity;
-		if (task is null)
-		{
-			return;
-		}
+    public async ValueTask Delete(CancellationToken ct)
+    {
+        var task = await Entity;
+        if (task is null)
+        {
+            return;
+        }
 
-		var result = await _navigator.ShowMessageDialogAsync<object>(this, Dialog.ConfirmDeleteTask, cancellation: ct);
-		if (result == DialogResults.Affirmative)
-		{
-			await _svc.DeleteAsync(task, ct);
-			await _navigator.NavigateBackAsync(this, cancellation: ct);
-		}
-	}
+        var result = await _navigator.ShowMessageDialogAsync<object>(this, Dialog.ConfirmDeleteTask, cancellation: ct);
+        if (result == DialogResults.Affirmative)
+        {
+            await _svc.DeleteAsync(task, ct);
+            await _navigator.NavigateBackAsync(this, cancellation: ct);
+        }
+    }
 
-	public async ValueTask DeleteDueDate(CancellationToken ct)
-	{
-		if (await Entity is { } task)
-		{
-			await _svc.UpdateAsync(task with { DueDateTime = null }, ct);
-		}
-	}
+    public async ValueTask DeleteDueDate(CancellationToken ct)
+    {
+        if (await Entity is { } task)
+        {
+            await _svc.UpdateAsync(task with { DueDateTime = null }, ct);
+        }
+    }
 
-	public async ValueTask AddDueDate(CancellationToken ct)
-	{
-		var task = await Entity;
-		if (task is null)
-		{
-			return;
-		}
+    public async ValueTask AddDueDate(CancellationToken ct)
+    {
+        var task = await Entity;
+        if (task is null)
+        {
+            return;
+        }
 
-		var result = await _navigator
-			.NavigateViewModelForResultAsync<ExpirationDateViewModel, PickedDate>(this, qualifier: Qualifiers.Dialog, data: new PickedDate(task.DueDateTime), cancellation: ct)
-			.AsResult();
+        var result = await _navigator
+            .NavigateViewModelForResultAsync<ExpirationDateViewModel, PickedDate>(this, qualifier: Qualifiers.Dialog, data: new PickedDate(task.DueDateTime), cancellation: ct)
+            .AsResult();
 
-		if (result.SomeOrDefault()?.Date is { } date)
-		{
-			await _svc.UpdateAsync(task with { DueDateTime = date }, ct);
-			await Entity.UpdateValue(opt => opt.Map(task => task with { DueDateTime = date }), ct);
-		}
-	}
+        if (result.SomeOrDefault()?.Date is { } date)
+        {
+            await _svc.UpdateAsync(task with { DueDateTime = date }, ct);
+            await Entity.UpdateValue(opt => opt.Map(task => task with { DueDateTime = date }), ct);
+        }
+    }
 }
