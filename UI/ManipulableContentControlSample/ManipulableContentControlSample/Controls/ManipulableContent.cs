ff35d14682e2b0d3e33d90092dc3631e40fa7db7
Iury Carlos <iurycs@gmail.com>
2024-04-03 17:36:55 -0300

    chore: still missing refinement

--- UI/ManipulableContentControlSample/ManipulableContentControlSample/Controls/ManipulableContent.cs
+++ UI/ManipulableContentControlSample/ManipulableContentControlSample/Controls/ManipulableContent.cs
@@ -1,13 +1,19 @@
 using Microsoft.UI.Input;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Windows.Foundation;
 
 namespace ManipulableContentControlSample.Controls;
 
 [TemplatePart(Name = "PART_Presenter", Type = typeof(ContentPresenter))]
+[TemplatePart(Name = "PART_scrollV", Type = typeof(ScrollBar))]
+[TemplatePart(Name = "PART_scrollH", Type = typeof(ScrollBar))]
 public partial class ManipulableContent : ContentControl
 {
     private ContentPresenter? _presenter;
+    private ScrollBar? _scrollV;
+    private ScrollBar? _scrollH;
 
     #region Dependency Properties
     public static readonly DependencyProperty IsActiveProperty =
@@ -45,6 +51,24 @@ public partial class ManipulableContent : ContentControl
 
     public static readonly DependencyProperty VerticalOffsetProperty =
     DependencyProperty.Register(nameof(VerticalOffset), typeof(double), typeof(ManipulableContent), new PropertyMetadata(0.0d));
+
+    public static readonly DependencyProperty VerticalMaxScrollProperty =
+    DependencyProperty.Register(nameof(VerticalMaxScroll), typeof(double), typeof(ManipulableContent), new PropertyMetadata(0.0d));
+
+    public static readonly DependencyProperty VerticalMinScrollProperty =
+    DependencyProperty.Register(nameof(VerticalMinScroll), typeof(double), typeof(ManipulableContent), new PropertyMetadata(0.0d));
+
+    public static readonly DependencyProperty HorizontalMaxScrollProperty =
+    DependencyProperty.Register(nameof(HorizontalMaxScroll), typeof(double), typeof(ManipulableContent), new PropertyMetadata(0.0d));
+
+    public static readonly DependencyProperty HorizontalMinScrollProperty =
+    DependencyProperty.Register(nameof(HorizontalMinScroll), typeof(double), typeof(ManipulableContent), new PropertyMetadata(0.0d));
+
+    public static readonly DependencyProperty ViewPortWidthProperty =
+    DependencyProperty.Register(nameof(ViewPortWidth), typeof(double), typeof(ManipulableContent), new PropertyMetadata(0.0d));
+
+    public static readonly DependencyProperty ViewPortHeightProperty =
+    DependencyProperty.Register(nameof(ViewPortHeight), typeof(double), typeof(ManipulableContent), new PropertyMetadata(0.0d));    
 
     public bool IsActive
     {
@@ -117,6 +141,38 @@ public partial class ManipulableContent : ContentControl
         get => (double)GetValue(VerticalOffsetProperty);
         set => SetValue(VerticalOffsetProperty, value);
     }
+
+    public double VerticalMaxScroll
+    {
+        get => (double)GetValue(VerticalMaxScrollProperty);
+        set => SetValue(VerticalMaxScrollProperty, value);
+    }
+    public double VerticalMinScroll
+    {
+        get => (double)GetValue(VerticalMinScrollProperty);
+        set => SetValue(VerticalMinScrollProperty, value);
+    }
+    public double HorizontalMaxScroll
+    {
+        get => (double)GetValue(HorizontalMaxScrollProperty);
+        set => SetValue(HorizontalMaxScrollProperty, value);
+    }
+    public double HorizontalMinScroll
+    {
+        get => (double)GetValue(HorizontalMinScrollProperty);
+        set => SetValue(HorizontalMinScrollProperty, value);
+    }
+    public double ViewPortHeight
+    {
+        get => (double)GetValue(ViewPortHeightProperty);
+        set => SetValue(ViewPortHeightProperty, value);
+    }
+    public double ViewPortWidth
+    {
+        get => (double)GetValue(ViewPortWidthProperty);
+        set => SetValue(ViewPortWidthProperty, value);
+    }
+
     #endregion
 
     private void RegisterPropertyHandlers()
@@ -125,6 +181,18 @@ public partial class ManipulableContent : ContentControl
         RegisterPropertyChangedCallback(ZoomLevelProperty, CoerceZoomLevel);
         RegisterPropertyChangedCallback(MinZoomLevelProperty, CoerceZoomLevel);
         RegisterPropertyChangedCallback(MaxZoomLevelProperty, CoerceZoomLevel);
+
+        RegisterPropertyChangedCallback(ZoomLevelProperty, (s, e) => { UpdateScrollLimits();}) ;
+    }
+
+    private void UpdateScrollLimits()
+    {
+        //min and max are inverted to slide on the opposite direction of the drag
+        HorizontalMinScroll = this.ActualWidth * ZoomLevel;
+        VerticalMinScroll = this.ActualHeight * ZoomLevel;
+
+        HorizontalMaxScroll = -1 * HorizontalMinScroll;
+        VerticalMaxScroll = -1 * VerticalMinScroll;
     }
 
     private void CoerceZoomLevel(DependencyObject sender, DependencyProperty dp)
@@ -137,14 +205,76 @@ public partial class ManipulableContent : ContentControl
     {
         DefaultStyleKey = typeof(ManipulableContent);
 
+        this.Loaded += (s, e) =>
+        {
+            if (_presenter?.Content is FrameworkElement { } fe)
+            {
+                fe.LayoutUpdated += (s, e) =>
+                {
+                    ViewPortWidth = fe.ActualWidth;
+                    ViewPortHeight = fe.ActualHeight;
+
+                    UpdateScrollLimits();
+                };
+            }
+        };
         RegisterPropertyHandlers();
     }
 
     protected override void OnApplyTemplate()
     {
         _presenter = GetTemplateChild("PART_Presenter") as ContentPresenter;
+        _scrollV = GetTemplateChild("PART_scrollV") as ScrollBar;
+        _scrollH = GetTemplateChild("PART_scrollH") as ScrollBar;
+
+        RegisterToControlEvents();
+        ResetOffset();
+        ResetZoom();
         RegisterPointerHandlers();
     }
+
+    private void RegisterToControlEvents()
+    {
+        //due to templatebinding there's no TwoWay mode. We need to manually update the values
+        if (_scrollV is not null)
+        {
+            _scrollV.Scroll += _scrollV_Scroll;
+        }
+
+        if (_scrollH is not null)
+        {
+             _scrollH.Scroll += _scrollH_Scroll;
+        }
+    }
+
+    private void _scrollV_Scroll(object sender, ScrollEventArgs e)
+    {
+        VerticalOffset = e.NewValue;
+    }
+
+    private void _scrollH_Scroll(object sender, ScrollEventArgs e)
+    {
+        HorizontalOffset = e.NewValue;
+    }
+
+    private void ApplyBindings()
+    {
+        if (_scrollV is not null)
+        {
+            var sVBinding = new Binding { Path = new PropertyPath("VerticalOffset"), Mode = BindingMode.TwoWay};
+            sVBinding.Source = this; 
+            _scrollV.SetBinding(ScrollBar.ValueProperty, sVBinding);
+        }
+
+        if (_scrollH is not null)
+        {
+            var sHBinding = new Binding { Path = new PropertyPath("HorizontalOffset"), Mode = BindingMode.TwoWay };
+            sHBinding.Source = this;
+            _scrollH.SetBinding(ScrollBar.ValueProperty, sHBinding);
+        }
+    }
+
+
     private uint _capturedPointerId;
     private Point _referencePosition;
 
@@ -277,12 +407,20 @@ public partial class ManipulableContent : ContentControl
 
     public void ResetZoom()
     {
-        ZoomLevel = 1;
+        if(IsAllowedToWork)
+        {
+            ZoomLevel = 1;
+            HorizontalZoomCenter = 0;
+            VerticalZoomCenter = 0;
+        }
     }
 
     public void ResetOffset()
     {
-        HorizontalOffset = 0;
-        VerticalOffset = 0;
+        if(IsAllowedToWork)
+        {
+            HorizontalOffset = 0;
+            VerticalOffset = 0;
+        }
     }
 }
