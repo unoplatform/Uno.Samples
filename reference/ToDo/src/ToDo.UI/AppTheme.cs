d9fdf2f474f95fb23a886840a2e2826ef1f5fdb5
Nick Randolph <nick@builttoroam.com>
2024-04-08 13:44:58 +1000

    chore: Fix nullability warning

--- reference/ToDo/src/ToDo.UI/AppTheme.cs
+++ reference/ToDo/src/ToDo.UI/AppTheme.cs
@@ -4,20 +4,20 @@
 // See https://github.com/unoplatform/uno.extensions/discussions/420
 public class AppTheme : IAppTheme
 {
-	private readonly Window _window;
-	private readonly IDispatcher _dispatcher;
-	public AppTheme(Window window, IDispatcher dispatcher)
-	{
-		_window = window;
-		_dispatcher = dispatcher;
-	}
-	public bool IsDark => SystemThemeHelper.IsRootInDarkMode(_window.Content.XamlRoot!);
+    private readonly Window _window;
+    private readonly IDispatcher _dispatcher;
+    public AppTheme(Window window, IDispatcher dispatcher)
+    {
+        _window = window;
+        _dispatcher = dispatcher;
+    }
+    public bool IsDark => SystemThemeHelper.IsRootInDarkMode(_window.Content?.XamlRoot!);
 
-	public async Task SetThemeAsync(bool darkMode)
-	{
-		await _dispatcher.ExecuteAsync(() =>
-		{
-			SystemThemeHelper.SetRootTheme(_window.Content.XamlRoot, darkMode);
-		});
-	}
+    public async Task SetThemeAsync(bool darkMode)
+    {
+        await _dispatcher.ExecuteAsync(() =>
+        {
+            SystemThemeHelper.SetRootTheme(_window.Content?.XamlRoot, darkMode);
+        });
+    }
 }
