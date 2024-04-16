8ef3118d8dc9eb4feaf455f26b2ea8a2ae72f954
Laurent Proulx <proulx.laurent@gmail.com>
2024-04-16 14:23:35 -0400

    feat: create new csharp-mvux Counter project

---
+++ reference/Counter/CSharp-MVUX/Counter/Counter/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using System.Collections.Immutable;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
+global using Color = Windows.UI.Color;
---
+++ reference/Counter/CSharp-MVUX/Counter/Counter/MainPage.cs
@@ -0,0 +1,17 @@
+namespace Counter;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this
+            .Background(ThemeResource.Get<Brush>("ApplicationPageBackgroundThemeBrush"))
+            .Content(new StackPanel()
+            .VerticalAlignment(VerticalAlignment.Center)
+            .HorizontalAlignment(HorizontalAlignment.Center)
+            .Children(
+                new TextBlock()
+                    .Text("Hello Uno Platform!")
+            ));
+    }
+}
