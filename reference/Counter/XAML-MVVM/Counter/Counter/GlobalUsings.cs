8634890426a145548f921b9fc2c07f75a02ac46e
Laurent Proulx <proulx.laurent@gmail.com>
2024-04-16 15:36:22 -0400

    feat : add XAML-MVVM Counter App

---
+++ reference/Counter/XAML-MVVM/Counter/Counter/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System.Collections.Immutable;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
---
+++ reference/Counter/XAML-MVVM/Counter/Counter/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Counter;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+}
