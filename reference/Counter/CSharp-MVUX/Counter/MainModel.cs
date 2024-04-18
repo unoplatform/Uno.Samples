dda9da7163053501992f07935fce880ea91b1383
Laurent Proulx <proulx.laurent@gmail.com>
2024-04-18 11:43:12 -0400

    feat Add Single Counter App

---
+++ reference/Counter/CSharp-MVUX/Counter/MainModel.cs
@@ -0,0 +1,11 @@
+namespace Counter;
+
+internal partial record MainModel
+{
+    public IState<int> Count => State.Value(this, () => 0);
+
+    public IState<int> Step => State.Value(this, () => 1);
+
+    public ValueTask IncrementCommand(int Step)
+            => Count.Update(c => c + Step, CancellationToken.None);
+}
---
+++ reference/Counter/XAML-MVVM/Counter/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System.Collections.Immutable;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
