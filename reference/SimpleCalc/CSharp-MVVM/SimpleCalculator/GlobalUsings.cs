a10ac8c17ae756eacb239431ea53a111fe30dbea
Laurent Proulx <proulx.laurent@gmail.com>
2024-04-19 08:57:42 -0400

    feat: add 4 version of single app simple calculator

---
+++ reference/SimpleCalc/CSharp-MVVM/SimpleCalculator/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System.Collections.Immutable;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
+global using Color = Windows.UI.Color;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
