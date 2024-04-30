b4f42aa3bc647a76711d0b2d56a1e140f6718819
Martin Zikmund <martin@zikmund.dev>
2024-04-30 15:24:45 +0200

    chore: Improved basic window sample

--- UI/WindowingSamples/WindowingSamples/MainPage.xaml.cs
+++ UI/WindowingSamples/WindowingSamples/MainPage.xaml.cs
@@ -11,19 +11,39 @@ public sealed partial class MainPage : Page
 
     public void BasicWindowSample()
     {
+        // Create window instance
         var window = new Window();
-        var textBlock = new TextBlock() { Text = "Hello, from second window!", FontSize = 60 };
-        window.Content = textBlock;
+
+        // Set up content
+        var grid = new Grid()
+        {
+            Background = new SolidColorBrush(Colors.CornflowerBlue),
+            Children =
+            {
+                new TextBlock()
+                {
+                    Text = "Hello from second window!",
+                    Foreground = new SolidColorBrush(Colors.White),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    FontSize = 60
+                }
+            }
+        };
+
+        window.Content = grid;
+
+        // Show window
         window.Activate();
     }
 
     public void CustomWindowSample() { } // => new CustomWindow().Activate();
 
-    public void FullScreenModeSample() => new FullScreenModeWindow().Activate();
+public void FullScreenModeSample() => new FullScreenModeWindow().Activate();
 
-    public void MinimizeMaximizeSample() => new MinimizeMaximizeWindow().Activate();
+public void MinimizeMaximizeSample() => new MinimizeMaximizeWindow().Activate();
 
-    public void WindowTitleSample() => new WindowTitleWindow().Activate();
+public void WindowTitleSample() => new WindowTitleWindow().Activate();
 
-    public void StayOnTopSample() => new StayOnTopWindow().Activate();
+public void StayOnTopSample() => new StayOnTopWindow().Activate();
 }
