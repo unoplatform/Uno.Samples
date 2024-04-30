b8e3556e420617e4cef8fe5dbd2d49c608f396a8
Martin Zikmund <martin@zikmund.dev>
2024-04-30 15:06:09 +0200

    chore: Full screen mode sample

---
+++ UI/WindowingSamples/WindowingSamples/FullScreenModeWindow.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.UI.Windowing;
+
+namespace WindowingSamples;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class FullScreenModeWindow : Window
+{
+    public FullScreenModeWindow()
+    {
+        InitializeComponent();
+    }
+}
