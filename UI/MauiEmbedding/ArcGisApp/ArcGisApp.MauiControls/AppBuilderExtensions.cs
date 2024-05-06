c8748b33f1ae01bc1ec737ee518056c584351ef5
Vatsa Shah <vatsashah45@gmail.com>
2024-05-06 18:05:44 -0400

    feat: MauiCOntrols migration

--- UI/MauiEmbedding/ArcGisApp/ArcGisApp.MauiControls/AppBuilderExtensions.cs
+++ UI/MauiEmbedding/ArcGisApp/ArcGisApp.MauiControls/AppBuilderExtensions.cs
@@ -1,3 +1,7 @@
+using Esri.ArcGISRuntime;
+using Esri.ArcGISRuntime.Maui;
+using Esri.ArcGISRuntime.Security;
+
 namespace ArcGisApp;
 
 public static class AppBuilderExtensions
@@ -6,7 +10,15 @@ public static class AppBuilderExtensions
         builder
             .ConfigureFonts(fonts =>
             {
-                fonts.AddFont("Assets/Fonts/OpenSansRegular.ttf", "OpenSansRegular");
-                fonts.AddFont("Assets/Fonts/OpenSansSemibold.ttf", "OpenSansSemibold");
-            });
+                fonts.AddFont("ArcGisApp/Assets/Fonts/OpenSansRegular.ttf", "OpenSansRegular");
+                fonts.AddFont("ArcGisApp/Assets/Fonts/OpenSansSemibold.ttf", "OpenSansSemibold");
+            })
+            .UseArcGISRuntime(
+            //config => config
+            //    .UseLicense("[Your ArcGIS Maps SDK License key]")
+            //    .UseApiKey("[Your ArcGIS location services API Key]")
+            //    .ConfigureAuthentication(auth => auth
+            //        .UseDefaultChallengeHandler() // Use the default authentication dialog
+            //    )
+            );
 }
