fc571239497d8c237cfa8a5fbee517a16238398b
Kunal22shah <kunalshah606@gmail.com>
2024-06-06 15:31:30 -0400

    fix: snackBar issue

--- UI/MauiEmbedding/MauiCommunityToolkitApp/MauiCommunityToolkitApp.MauiControls/AppBuilderExtensions.cs
+++ UI/MauiEmbedding/MauiCommunityToolkitApp/MauiCommunityToolkitApp.MauiControls/AppBuilderExtensions.cs
@@ -7,6 +7,7 @@ public static class AppBuilderExtensions
     public static MauiAppBuilder UseMauiControls(this MauiAppBuilder builder)
         => builder
             .UseMauiCommunityToolkit()
+            .UseMauiCommunityToolkit(options => options.SetShouldEnableSnackbarOnWindows(true))
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("MauiCommunityToolkitApp/Assets/Fonts/OpenSansRegular.ttf", "OpenSansRegular");
