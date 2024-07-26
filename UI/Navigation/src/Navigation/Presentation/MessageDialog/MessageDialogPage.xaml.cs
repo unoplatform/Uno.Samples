5c11d1066890878a3f85db61f3b3f3cbf921d925
vatsashah45 <vatsashah45@gmail.com>
2024-07-26 11:09:08 -0400

    feat: Dialog override, disappear, flyout and complex flyout

--- UI/Navigation/src/Navigation/Presentation/MessageDialog/MessageDialogPage.xaml.cs
+++ UI/Navigation/src/Navigation/Presentation/MessageDialog/MessageDialogPage.xaml.cs
@@ -6,4 +6,17 @@ public sealed partial class MessageDialogPage : Page
 	{
 		this.InitializeComponent();
 	}
+
+	private async void MessageDialogCodebehindRouteOverrideClick(object sender, RoutedEventArgs args)
+	{
+		var messageDialogResult = await this.Navigator()!.ShowMessageDialogAsync<string>(this, route: "LocalizedConfirm", content: "Override content", title: "Override title");
+		MessageDialogResultText.Text = $"Message dialog result: {messageDialogResult}";
+	}
+
+	private async void MessageDialogCodebehindCancelClick(object sender, RoutedEventArgs args)
+	{
+		var cancelSource = new CancellationTokenSource(TimeSpan.FromSeconds(2));
+		var messageDialogResult = await this.Navigator()!.ShowMessageDialogAsync<string>(this, content: "This is Content", title: "This is title", cancellation: cancelSource.Token);
+		MessageDialogResultText.Text = $"Message dialog result: {messageDialogResult}";
+	}
 }
