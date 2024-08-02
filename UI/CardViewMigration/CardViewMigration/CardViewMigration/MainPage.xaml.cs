b6ba010d537ce874f3f76109fdc15e4be164a67a
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2024-08-02 17:37:10 +0300

    chore: Adjust

--- UI/CardViewMigration/CardViewMigration/CardViewMigration/MainPage.xaml.cs
+++ UI/CardViewMigration/CardViewMigration/CardViewMigration/MainPage.xaml.cs
@@ -1,16 +1,8 @@
 using CommunityToolkit.Mvvm.Input;
-using Microsoft.UI.Xaml.Controls;
-using System;
 using System.Reflection;
-using System.Windows.Input;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace CardViewMigration;
 
-/// <summary>
-/// An empty page that can be used on its own or navigated to within a Frame.
-/// </summary>
 public sealed partial class MainPage : Page
 {
     public ICommand NavigateCommand { get; set; }
