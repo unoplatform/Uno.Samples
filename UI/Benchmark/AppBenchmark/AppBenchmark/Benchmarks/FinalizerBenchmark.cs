70c3102e0ff5c5f6c89d59d1a940aef97d71ffbc
eriklimakc <erik.lima@knowcode.app>
2024-10-03 13:06:55 +0100

    chore: Update Benchmark to Uno.Sdk 5.4

--- UI/Benchmark/AppBenchmark/AppBenchmark/Benchmarks/FinalizerBenchmark.cs
+++ UI/Benchmark/AppBenchmark/AppBenchmark/Benchmarks/FinalizerBenchmark.cs
@@ -1,43 +1,39 @@
-﻿using BenchmarkDotNet.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using BenchmarkDotNet.Attributes;
 
-namespace SamplesApp.Benchmarks.Suite.FinalizerBench
+namespace SamplesApp.Benchmarks.Suite.FinalizerBench;
+
+public class FinalizerBenchmark
 {
-    public class FinalizerBenchmark
+    [Benchmark(Baseline = true)]
+    public void NewHandles()
     {
-        [Benchmark(Baseline = true)]
-        public void NewHandles()
+        for (int i = 0; i < 100; i++)
         {
-            for (int i = 0; i < 100; i++)
-            {
-                new WithFinalizer();
-            }
+            new WithFinalizer();
+        }
+    }
+
+
+    [Benchmark()]
+    public void ReuseHandle()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            new WithoutFinalizer();
         }
 
+    }
 
-        [Benchmark()]
-        public void ReuseHandle()
-        {
-            for (int i = 0; i < 100; i++)
-            {
-                new WithoutFinalizer();
-            }
-
-        }
-
-        private class WithFinalizer
-        {
-            ~WithFinalizer()
-            {
-
-            }
-        }
-
-        private class WithoutFinalizer
+    private class WithFinalizer
+    {
+        ~WithFinalizer()
         {
 
         }
+    }
+
+    private class WithoutFinalizer
+    {
+
     }
 }
--- UI/Benchmark/AppBenchmark/AppBenchmark/Benchmarks/SpanBenchmark.cs
+++ UI/Benchmark/AppBenchmark/AppBenchmark/Benchmarks/SpanBenchmark.cs
@@ -1,46 +1,41 @@
-﻿using BenchmarkDotNet.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using BenchmarkDotNet.Attributes;
 
-namespace SamplesApp.Benchmarks.Suite.SpanBench
+namespace SamplesApp.Benchmarks.Suite.SpanBench;
+
+public class SpanTesting
 {
-    public class SpanTesting
+    [Params(10, 20)]
+    public int Items { get; set; }
+
+    [Benchmark(Baseline = true)]
+    public void EnumerableSum()
     {
-        [Params(10, 20)]
-        public int Items { get; set; }
+        var r = Enumerable.Range(0, Items).ToArray();
 
-        [Benchmark(Baseline = true)]
-        public void EnumerableSum()
-        {
-            var r = Enumerable.Range(0, Items).ToArray();
-
-            var s = r.Sum();
-        }
-
-
-        [Benchmark()]
-        public void SpanSum()
-        {
-            var r = Enumerable.Range(0, Items).ToArray();
-
-            var s = ((Span<int>)r).Sum();
-        }
+        var s = r.Sum();
     }
 
-    public static class Extensions
+
+    [Benchmark()]
+    public void SpanSum()
     {
-        public static double Sum(this Span<int> span)
+        var r = Enumerable.Range(0, Items).ToArray();
+
+        var s = ((Span<int>)r).Sum();
+    }
+}
+
+public static class Extensions
+{
+    public static double Sum(this Span<int> span)
+    {
+        double result = 0;
+
+        foreach (var value in span)
         {
-            double result = 0;
-
-            foreach (var value in span)
-            {
-                result += value;
-            }
-
-            return result;
+            result += value;
         }
+
+        return result;
     }
 }
