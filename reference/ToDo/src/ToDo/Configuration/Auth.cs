1b3d19d128850cd5c76f5afacfb2a73cb50a4e7e
DevTKSS <info@technische-konstruktion.com>
2025-04-01 08:03:04 +0200

    chore(deps): updated PackageReference to recommended Microsoft.IdentitityModel.JsonWebTokens

--- reference/ToDo/src/ToDo/Configuration/Auth.cs
+++ reference/ToDo/src/ToDo/Configuration/Auth.cs
@@ -1,9 +1,14 @@
-﻿namespace ToDo.Configuration;
+namespace ToDo.Configuration;
 
+// <see href="https://platform.uno/docs/articles/external/uno.extensions/doc/Learn/Serialization/HowTo-Serialization.html" />
 public partial record Auth
 {
-	public string? ApplicationId { get; init; }
-	public string[]? Scopes { get; init; }
-	public string? RedirectUri { get; init; }
-	public string? KeychainSecurityGroup { get; init; }
+    public string? ApplicationId { get; init; }
+    public string[]? Scopes { get; init; }
+    public string? RedirectUri { get; init; }
+    public string? KeychainSecurityGroup { get; init; }
 }
+
+[JsonSerializable(typeof(Auth))]
+internal partial class AuthJsonContext : JsonSerializerContext
+{ }
