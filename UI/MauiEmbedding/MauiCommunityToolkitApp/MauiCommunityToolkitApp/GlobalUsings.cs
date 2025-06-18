5588ace2593a52705196c044d0c60d42b8ca6bbd
Steve Bilogan <steve.bilogan@gmail.com>
2025-06-18 11:56:40 -0400

    chore: package bump

--- UI/MauiEmbedding/MauiCommunityToolkitApp/MauiCommunityToolkitApp/GlobalUsings.cs
+++ UI/MauiEmbedding/MauiCommunityToolkitApp/MauiCommunityToolkitApp/GlobalUsings.cs
@@ -1,6 +1,7 @@
 global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
 global using MauiCommunityToolkitApp.Presentation;
+global using Uno.Resizetizer;
 #if MAUI_EMBEDDING
 global using MauiCommunityToolkitApp.MauiControls;
 #endif
