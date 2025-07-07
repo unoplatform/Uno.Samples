c0dc9d834dc70b861343b1a44ac9a4a4e22d8794
Andres Pineda <pineda.andres@gmail.com>
2025-07-07 18:04:59 -0400

    chore: add mvux combined with mvvm sample

---
+++ UI/CombiningMVUXAndMVVM/CombiningMVUXAndMVVM/Presentation/MainModel.cs
@@ -0,0 +1,28 @@
+namespace CombiningMVUXAndMVVM.Presentation;
+
+public partial record MainModelOld
+{
+    private INavigator _navigator;
+
+    public MainModelOld(
+        IStringLocalizer localizer,
+        IOptions<AppConfig> appInfo,
+        INavigator navigator)
+    {
+        _navigator = navigator;
+        Title = "Main";
+        Title += $" - {localizer["ApplicationName"]}";
+        Title += $" - {appInfo?.Value?.Environment}";
+    }
+
+    public string? Title { get; }
+
+    public IState<string> Name => State<string>.Value(this, () => string.Empty);
+
+    public async Task GoToSecond()
+    {
+        var name = await Name;
+        await _navigator.NavigateViewModelAsync<SecondModel>(this, data: new Entity(name!));
+    }
+
+}
